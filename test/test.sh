09e3f7947b4a43668a5841f522a7cbde37dd8ba5
matken11235 <matken11235@gmail.com>
2018-01-29 05:22:22 +0900

    first commit

---
+++ test/test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# print_summaries() {
+#   for command; do
+#     echo "$command"
+#   done
+# }
+
+# print_summaries commands local global shell install uninstall rehash version versions which whence
+
+
+# containsElement () {
+#   local e="$2"
+#   # shift
+#   # echo $ma
+#   for e; do [[ "$e" == "$1" ]] && return 0 || return 1; done
+# }
+
+# declare -a commands=('help' 'version' 'init' 'new' 'install')
+
+# # # containsElement 'hep' "${commands[@]}"
+# # # echo $?
+# # case "${commands[@]}" in *"he"*) echo "found" ;; esac
+
+# isin () { for e in "${@:2}"; do [[ "$e" = "$1" ]] && return '0' || return '1'; done; }
+# ok=`isin 'help' "${commands[@]}" && echo $?`
+# echo $ok
+
+touch test.txt
