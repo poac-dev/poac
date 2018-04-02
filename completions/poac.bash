3f23d7223d27e2bac36fa8df565ab4a002fb40e9
matken11235 <matken11235@gmail.com>
2018-04-03 04:44:58 +0900

    やっぱりC++で書くことにした

--- completions/poac.bash
+++ completions/poac.bash
@@ -1,6 +1,6 @@
-#
+#!/bin/bash
 
-__poac() {
+__poac () {
   local cur=${COMP_WORDS[COMP_CWORD]}
   local prev=${COMP_WORDS[COMP_CWORD-1]}
   local opts="$(find $(poac root)/libexec -maxdepth 1 -type f -name 'poac-*' | awk -F '/' '{print $NF}' | awk -F 'poac-' '{ print $2 }')"
--- libcpp/poac-search.cpp
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Summary: 
-# Options: 
-#
-set -e
-
-
