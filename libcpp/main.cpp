808b4b87bc84382a55d754f73c4ca94ae889269e
matken11235 <matken11235@gmail.com>
2018-04-09 00:09:28 +0900

    initを実装

--- libcpp/main.cpp
+++ libcpp/main.cpp
@@ -8,12 +8,9 @@
 
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv) {
-    std::string s{ "root" };
 //    if (argc > 1) poac::inference::exec(argv[1]);
     try {
-        // TODO: この問題の解決策は．特殊化しかない．execのif分岐で実装？？？
-        poac::inference::exec("--help");
-        poac::inference::help();
+        poac::inference::exec("init");
     }
     catch (const std::invalid_argument& e) {
         std::cerr << e.what();
