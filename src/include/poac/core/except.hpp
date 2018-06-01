3e752b94de2119dd514c7363f20294719686d600
matken11235 <matken11235@gmail.com>
2018-06-01 21:31:21 +0900

    std::runtime_error -> std::invalid_argument

--- src/include/poac/core/except.hpp
+++ src/include/poac/core/except.hpp
@@ -6,19 +6,19 @@
 
 
 namespace poac::core {
-    class invalid_first_argument : public std::runtime_error
+    class invalid_first_argument : public std::invalid_argument
     {
     public:
-        explicit invalid_first_argument(const std::string& __s) : runtime_error(__s) {}
-        explicit invalid_first_argument(const char* __s)        : runtime_error(__s) {}
+        explicit invalid_first_argument(const std::string& __s) : invalid_argument(__s) {}
+        explicit invalid_first_argument(const char* __s)        : invalid_argument(__s) {}
 
         virtual ~invalid_first_argument() {};
     };
-    class invalid_second_argument : public std::runtime_error
+    class invalid_second_argument : public std::invalid_argument
     {
     public:
-        explicit invalid_second_argument(const std::string& __s) : runtime_error(__s) {}
-        explicit invalid_second_argument(const char* __s)        : runtime_error(__s) {}
+        explicit invalid_second_argument(const std::string& __s) : invalid_argument(__s) {}
+        explicit invalid_second_argument(const char* __s)        : invalid_argument(__s) {}
 
         virtual ~invalid_second_argument() {};
     };
