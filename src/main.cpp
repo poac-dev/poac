762bd7251ba0231bd9e874aae5b8a44ee8203cc7
matken11235 <matken11235@gmail.com>
2018-06-01 21:28:17 +0900

    fix #12

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+#include <cstring>
 #include <cstdlib>
 
 #include "include/poac.hpp"
@@ -27,8 +28,7 @@ int exec(S&& s, VS&& vs) {
         return error_handling(e.what());
     }
     catch (const poac::core::invalid_second_argument& e) {
-        VS vs2 = { e.what() };
-        poac::inference::apply("exec", "--help", std::move(vs2));
+        poac::inference::apply("exec", "--help", std::move(VS({ e.what() })));
         return EXIT_FAILURE;
     }
     catch (...) {
@@ -36,9 +36,19 @@ int exec(S&& s, VS&& vs) {
     }
 }
 
+bool equal_str(const std::string& s1, const std::string& s2, const std::string& s3) {
+    return (s1 == s2) || (s1 == s3);
+}
+
 int main(int argc, const char** argv) {
-    // argv[0]: poac, argv[1]: new, ...
-    if (argc >= 3) { return exec(argv[1], VS(argv+2, argv+argc)); }
+    // argv[0]: poac, argv[1]: install, argv[2]: 1, ...
+
+    //$ poac install --help => exec("--help", ["install"])
+    if (argc == 3 && equal_str(argv[2], "-h", "--help")) { return exec(argv[2], VS({ argv[1] })); }
+    //$ poac install 1 2 3 => exec("install", ["1", "2", "3"])
+    else if (argc >= 3) { return exec(argv[1], VS(argv+2, argv+argc)); }
+    //$ poac install => exec("install", [])
     else if (argc >= 2) { return exec(argv[1], VS()); }
-    else { poac::inference::apply("exec", "--help", VS()); return EXIT_FAILURE; }
+    //$ poac => exec("--help", [])
+    else { exec("--help", VS()); return EXIT_FAILURE; }
 }
