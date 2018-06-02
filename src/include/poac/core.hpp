5f8852091fe406a61272a4b4480ab11060cf7f24
matken11235 <matken11235@gmail.com>
2018-06-02 22:46:25 +0900

    Add SAT solver

--- src/include/poac/core.hpp
+++ src/include/poac/core.hpp
@@ -2,5 +2,6 @@
 #define POAC_CORE_HPP
 
 #include "core/except.hpp"
+#include "core/sat.hpp"
 
 #endif // !POAC_CORE_HPP
---
+++ src/include/poac/core/sat.hpp
@@ -0,0 +1,11 @@
+// Version SAT: https://research.swtch.com/version-sat
+#ifndef POAC_CORE_SAT_HPP
+#define POAC_CORE_SAT_HPP
+
+#include <string>
+
+
+namespace poac::core {
+
+} // end namespace
+#endif // !POAC_CORE_SAT_HPP
