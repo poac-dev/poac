1f7ebe68d0eb899325f9e1e33f6006bcc3797cc9
matken11235 <matken11235@gmail.com>
2018-06-17 15:07:36 +0900

    Create prototype of search sub-command

--- src/include/poac/subcmd/search.hpp
+++ src/include/poac/subcmd/search.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 
+#include "../utility/requests.hpp"
+
 
 namespace poac::subcmd { struct search {
     static const std::string summary() { return "Search for packages in poacpm."; }
@@ -12,6 +14,13 @@ namespace poac::subcmd { struct search {
     template <typename VS>
     void operator()(VS&& vs) { _main(vs); }
     template <typename VS>
-    void _main([[maybe_unused]] VS&& vs) {}
+    void _main([[maybe_unused]] VS&& vs) {
+        _search(vs[0]);
+    }
+
+    void _search(const std::string& word) {
+        const std::string url("https://poac.pm/api/v1/packages?search=" + word);
+        std::cout << poac::utility::requests::get(url) << std::endl;
+    }
 };} // end namespace
 #endif // !POAC_SUBCMD_SEARCH_HPP
--- src/include/poac/utility/requests.hpp
+++ src/include/poac/utility/requests.hpp
@@ -8,24 +8,23 @@
 
 
 namespace poac::utility::requests {
-    size_t callbackWrite(char *ptr, size_t size, size_t nmemb, std::string *stream) {
+    size_t callbackWrite(char* ptr, size_t size, size_t nmemb, std::string* stream) {
         int dataLength = size * nmemb;
         stream->append(ptr, dataLength);
         return dataLength;
     }
 
-    void get() {
+    std::string get(const std::string& url) {
         std::string chunk;
         if (CURL* curl = curl_easy_init(); curl != nullptr) {
-//            curl_easy_setopt(curl, CURLOPT_URL, "https://poac.pm/api/v1/");
-            curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.co.jp/");
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
             curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callbackWrite);
             curl_easy_setopt(curl, CURLOPT_WRITEDATA, &chunk);
             if (CURLcode ret = curl_easy_perform(curl); ret != CURLE_OK)
                 std::cerr << "curl_easy_perform() failed." << std::endl;
             curl_easy_cleanup(curl);
         }
-        std::cout << chunk << std::endl;
+        return chunk;
     }
 } // end namespace
 #endif // !POAC_UTILITY_REQUESTS_HPP
