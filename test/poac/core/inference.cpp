7e3c26a9ea78b500c2931c2e28c3671cfea6a3b8
matken11235 <matken11235@gmail.com>
2018-08-09 18:38:16 +0900

    mv test/poac/inference.cpp -> test/poac/core/inference.cpp

--- test/poac/core/inference.cpp
+++ test/poac/core/inference.cpp
@@ -3,11 +3,11 @@
 #include <vector>
 #include <string>
 
-#include "../../src/include/poac/core/inference.hpp"
-#include "../../src/include/poac/core/except.hpp"
+#include "../../../src/include/poac/core/inference.hpp"
+#include "../../../src/include/poac/core/except.hpp"
 
 
-BOOST_AUTO_TEST_CASE( test1 )
+BOOST_AUTO_TEST_CASE( poac_core_inference_test1 )
 {
     using namespace std;
     using namespace poac::core::inference;
