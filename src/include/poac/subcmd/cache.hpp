385fb9ad2f0de9731f1890356cfaabbd44f6dd8a
matken11235 <matken11235@gmail.com>
2018-08-10 09:35:11 +0900

    Implemented part of the function

--- src/include/poac/subcmd/cache.hpp
+++ src/include/poac/subcmd/cache.hpp
@@ -3,14 +3,13 @@
 
 #include <iostream>
 #include <string>
-#include <sstream>
-//#include <optional>
+//#include <csignal>trap "rm to_TFRecord.py" 0
 
-#include <boost/optional.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
 
 #include "../core/except.hpp"
-#include "../io/cli.hpp"
-#include "../io/network.hpp"
+#include "../io/file.hpp"
 
 
 namespace poac::subcmd { struct cache {
@@ -20,10 +19,39 @@ namespace poac::subcmd { struct cache {
         template <typename VS>
         void operator()(VS&& vs) { _main(vs); }
         template <typename VS>
-        void _main([[maybe_unused]] VS&& vs) {
-            // root
-            // list
-            // clean
+        void _main(VS&& argv) {
+            namespace except = core::except;
+
+            check_arguments(argv);
+            if (argv[0] == "root" && argv.size() == 1)
+                root();
+            else if (argv[0] == "list")
+                list(std::vector<std::string>(argv.begin()+1, argv.begin()+argv.size()));
+            else if (argv[0] == "clean")
+                clean(std::vector<std::string>(argv.begin()+1, argv.begin()+argv.size()));
+            else
+                throw except::invalid_second_arg("cache");
+        }
+
+        void clean(const std::vector<std::string>& argv) {
+            namespace fs = boost::filesystem;
+            if (argv.empty())
+                fs::remove_all(io::file::path::poac_cache_dir);
+        }
+
+        void list([[maybe_unused]] const std::vector<std::string>& argv) {
+            namespace fs = boost::filesystem;
+            for (const auto& e : boost::make_iterator_range(fs::directory_iterator(io::file::path::poac_cache_dir), {}))
+                std::cout << e.path().filename().string() << std::endl;
+        }
+
+        void root() {
+            std::cout << io::file::path::poac_cache_dir.string() << std::endl;
+        }
+
+        void check_arguments(const std::vector<std::string>& argv) {
+            namespace except = core::except;
+            if (argv.empty()) throw except::invalid_second_arg("cache");
         }
     };} // end namespace
 #endif // !POAC_SUBCMD_CACHE_HPP
