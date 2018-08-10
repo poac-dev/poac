07e4a2b1fc0ddc697571a448834e09a4d8795550
matken11235 <matken11235@gmail.com>
2018-08-10 13:59:15 +0900

    Implemented base of function

--- src/include/poac/subcmd/uninstall.hpp
+++ src/include/poac/subcmd/uninstall.hpp
@@ -1,7 +1,18 @@
+// Beta: Do not resolve dependencies
 #ifndef POAC_SUBCMD_UNINSTALL_HPP
 #define POAC_SUBCMD_UNINSTALL_HPP
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
+
+#include "../core/except.hpp"
+#include "../io/file/path.hpp"
+#include "../io/cli.hpp"
 
 
 namespace poac::subcmd { struct uninstall {
@@ -9,13 +20,29 @@ namespace poac::subcmd { struct uninstall {
     static const std::string options() { return "[<pkg-names>]"; }
 
     template <typename VS>
-    void operator()(VS&& vs) { _main(vs); }
+    void operator()(VS&& argv) { _main(argv); }
     template <typename VS>
-    void _main([[maybe_unused]] VS&& vs) {
+    void _main(VS&& argv) {
+        namespace fs = boost::filesystem;
         // Check if the deps directory exists.
         // Perform dependency resolution.
         // Uninstall packages in serial or parallel to the deps directory.
         //   If there is no package, it displays an error.
+
+        check_arguments(argv);
+
+        for (const auto& v : argv) {
+            const fs::path pkg = io::file::path::current_deps_dir / v;
+            if (io::file::path::validate_dir(pkg))
+                fs::remove_all(io::file::path::current_deps_dir / v);
+            else
+                std::cout << io::cli::red << v << " not found" << io::cli::reset << std::endl;
+        }
+    }
+
+    void check_arguments(const std::vector<std::string>& argv) {
+        namespace except = core::except;
+        if (argv.empty()) throw except::invalid_second_arg("uninstall");
     }
 };} // end namespace
 #endif // !POAC_SUBCMD_UNINSTALL_HPP
