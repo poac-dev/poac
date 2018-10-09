a652737e3f8aa55bfc5e8d73947a4776548888a6
matken11235 <matken11235@gmail.com>
2018-10-09 10:27:52 +0900

    Improve new sub command, support to org/name style

--- include/poac/subcmd/new.hpp
+++ include/poac/subcmd/new.hpp
@@ -13,60 +13,62 @@
 #include "../util/ftemplate.hpp"
 
 
-namespace poac::subcmd { struct new_ {
-    static const std::string summary() { return "Create a new poacpm project."; }
-    static const std::string options() { return "<project-name>"; }
+namespace poac::subcmd {
+    namespace _new {
+        void echo_info(const std::string& str) {
+            std::cout << io::cli::bold
+                      << "\n"
+                         "Your \"" + str + "\" project was created successfully.\n"
+                         "\n"
+                         "\n"
+                         "Go into your project by running:\n"
+                         "    $ cd " + str + "\n"
+                         "\n"
+                         "Start your project with:\n"
+                         "    $ poac run\n"
+                         "\n"
+                      << io::cli::reset;
+        }
 
-    template <typename VS, typename = std::enable_if_t<std::is_rvalue_reference_v<VS&&>>>
-    void operator()(VS&& argv) { _main(std::move(argv)); }
-    template <typename VS, typename = std::enable_if_t<std::is_rvalue_reference_v<VS&&>>>
-    void _main(VS&& argv) {
-        namespace fs     = boost::filesystem;
+        void exec_new(const std::string& dirname) {
+            namespace fs = boost::filesystem;
+            namespace path = io::file::path;
+            namespace ftmpl = util::ftemplate;
 
-        check_arguments(argv);
-        exec_new(argv[0]);
+            fs::create_directories(dirname);
+            std::ofstream ofs;
+            std::map<fs::path, std::string> file{
+                {".gitignore", ftmpl::_gitignore},
+                {"main.cpp",   ftmpl::main_cpp},
+                {"poac.yml",   ftmpl::poac_yml(dirname)},
+                {"README.md",  ftmpl::README_md(dirname)}
+            };
+            for (const auto& [name, text] : file)
+                path::write_to_file(ofs, (fs::path(dirname) / name).string(), text);
+            echo_info(dirname);
+        }
+
+        void check_arguments(const std::vector<std::string>& argv) {
+            namespace except = core::exception;
+            if (argv.size() != 1)
+                throw except::invalid_second_arg("new");
+            else if (io::file::path::validate_dir(argv[0]))
+                throw except::error("The " + argv[0] + " directory already exists.");
+        }
+
+        template<typename VS, typename = std::enable_if_t<std::is_rvalue_reference_v<VS&&>>>
+        void _main(VS&& argv) {
+            namespace fs = boost::filesystem;
+            check_arguments(argv);
+            exec_new(argv[0]);
+        }
     }
 
-    void check_arguments(const std::vector<std::string>& argv) {
-        namespace except = core::exception;
-        if (argv.size() != 1)
-            throw except::invalid_second_arg("new");
-        else if (io::file::path::validate_dir(argv[0]))
-            throw except::error("The "+argv[0]+" directory already exists.");
-    }
-
-    void exec_new(const std::string& dirname) {
-        namespace fs = boost::filesystem;
-        fs::create_directory(dirname);
-        std::ofstream ofs;
-        std::map<fs::path, std::string> file {
-                { ".gitignore", util::ftemplate::_gitignore },
-                { "main.cpp",   util::ftemplate::main_cpp },
-                { "poac.yml",   util::ftemplate::poac_yml(dirname) },
-                { "README.md",  util::ftemplate::README_md(dirname) }
-        };
-        for (const auto& [name, text] : file)
-            io::file::path::write_to_file(ofs, (fs::path(dirname)/name).string(), text);
-        echo_info(dirname);
-    }
-
-    void echo_info(const std::string& str) {
-        std::cout << io::cli::bold
-                  << info(str)
-                  << io::cli::reset;
-    }
-
-    std::string info(const std::string& str) {
-        return "\n"
-               "Your \""+str+"\" project was created successfully.\n"
-               "\n"
-               "\n"
-               "Go into your project by running:\n"
-               "    $ cd "+str+"\n"
-               "\n"
-               "Start your project with:\n"
-               "    $ poac run\n"
-               "\n";
-    }
-};} // end namespace
+    struct new_ {
+        static const std::string summary() { return "Create a new poacpm project."; }
+        static const std::string options() { return "<project-name>"; }
+        template<typename VS, typename = std::enable_if_t<std::is_rvalue_reference_v<VS&&>>>
+        void operator()(VS&& argv) { _new::_main(std::move(argv)); }
+    };
+} // end namespace
 #endif // !POAC_SUBCMD_NEW_HPP
