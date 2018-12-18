6e2700aa439007ab9690175e43e3d935e7a43235
matken11235 <matken11235@gmail.com>
2018-12-19 06:50:57 +0900

    Fix bug in root.hpp

--- include/poac/subcmd/root.hpp
+++ include/poac/subcmd/root.hpp
@@ -12,7 +12,7 @@ namespace poac::subcmd {
         static const std::string options() { return "<Nothing>"; }
         template<typename VS, typename = std::enable_if_t<std::is_rvalue_reference_v<VS&&>>>
         void operator()([[maybe_unused]] VS&& vs) {
-            std::cout << POAC_AUTO_DEF_PROJECT_ROOT << std::endl;
+            std::cout << POAC_PROJECT_ROOT << std::endl;
         }
     };
 } // end namespace
