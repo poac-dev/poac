2108fc9119026a3314963cd449f62c670f92b352
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-02-13 08:32:38 +0900

    Fix core/semver.hpp

--- include/poac/core/semver.hpp
+++ include/poac/core/semver.hpp
@@ -225,15 +225,10 @@ namespace poac::core::semver {
         return os;
     };
 
-    bool is_digit(char ch)
-    {
-        const int d = std::isdigit(static_cast<unsigned char>(ch));
-        return d;
-    }
     bool is_number(const std::string& s)
     {
         return !s.empty() && std::find_if(s.begin(), s.end(),
-                [](char c) { return !is_digit(c); }) == s.end();
+                [](unsigned char c) { return !std::isdigit(c); }) == s.end();
     }
 
     bool gt_pre(const Version& lhs, const Version& rhs) {
