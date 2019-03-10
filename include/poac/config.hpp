bfeee03bea9fc0cabec594b3b079c58489197895
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-03-11 03:33:01 +0900

    Remove static keyword in config.hpp

--- include/poac/config.hpp
+++ include/poac/config.hpp
@@ -10,30 +10,30 @@
 //#define DEBUG
 
 namespace poac {
-    static constexpr char const* POAC_API_HOST =
+    constexpr char const* POAC_API_HOST =
             "api.poac.pm";
-    static constexpr char const* POAC_ARCHIVE_API =
+    constexpr char const* POAC_ARCHIVE_API =
             "/packages/archive";
-    static constexpr char const* POAC_DEPS_API =
+    constexpr char const* POAC_DEPS_API =
             "/packages/deps";
-    static constexpr char const* POAC_EXISTS_API =
+    constexpr char const* POAC_EXISTS_API =
             "/packages/exists";
-    static constexpr char const* POAC_VERSIONS_API =
+    constexpr char const* POAC_VERSIONS_API =
             "/packages/versions";
-    static constexpr char const* POAC_UPLOAD_API =
+    constexpr char const* POAC_UPLOAD_API =
             "/packages/upload";
-    static constexpr char const* POAC_TOKENS_VALIDATE_API =
+    constexpr char const* POAC_TOKENS_VALIDATE_API =
             "/tokens/validate";
 
-    static constexpr char const* ALGOLIA_APPLICATION_ID =
+    constexpr char const* ALGOLIA_APPLICATION_ID =
             "IOCVK5FECM";
-    static constexpr char const* ALGOLIA_SEARCH_ONLY_KEY =
+    constexpr char const* ALGOLIA_SEARCH_ONLY_KEY =
             "9c0a76bacf692daa9e8eca2aaff4b2ab";
-    static constexpr char const* ALGOLIA_INDEX_NAME =
+    constexpr char const* ALGOLIA_INDEX_NAME =
             "packages";
-    static constexpr char const* ALGOLIA_SEARCH_INDEX_API_HOST =
+    constexpr char const* ALGOLIA_SEARCH_INDEX_API_HOST =
             "IOCVK5FECM-dsn.algolia.net";
-    static constexpr char const* ALGOLIA_SEARCH_INDEX_API =
+    constexpr char const* ALGOLIA_SEARCH_INDEX_API =
             "/1/indexes/packages/query";
 }
 #endif // !POAC_CONFIG_HPP
