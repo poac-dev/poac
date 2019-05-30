bfacdd9e845b5d10c25010cb0886d6dcccde331d
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-05-31 05:52:37 +0900

    Add test

--- test.sh
+++ test.sh
@@ -3,14 +3,16 @@
 ROOT_DIR=$PWD
 
 pushd ./test/poac/core
-
 g++ -std=c++1z -I${ROOT_DIR}/include -lboost_unit_test_framework -fprofile-arcs -ftest-coverage -o exception-test except.cpp
 ./exception-test
 g++ -std=c++1z -I${ROOT_DIR}/include -I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib -lboost_unit_test_framework -lboost_filesystem -lssl -lcrypto -ldl -lyaml-cpp -fprofile-arcs -ftest-coverage -DPOAC_VERSION=\"0.2.0\" -DPOAC_PROJECT_ROOT=\"${ROOT_DIR}\" -o infer-test infer.cpp
 ./infer-test
 g++ -std=c++1z -I${ROOT_DIR}/include -lboost_unit_test_framework -lboost_filesystem -lyaml-cpp -fprofile-arcs -ftest-coverage -o naming-test naming.cpp
 ./naming-test
+popd
 
+pushd ./test/poac/util
+g++ -std=c++1z -I${ROOT_DIR}/include -lboost_unit_test_framework -fprofile-arcs -ftest-coverage -o pretty-test pretty.cpp
 popd
 
 # Run gcov and upload report to coveralls
