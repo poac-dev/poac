01f8c09f19a85ef4f452606eb53c8f1fd5504529
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-05-31 02:17:08 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ cd ./test/poac/core
 g++ -std=c++1z -I${ROOT_DIR}/include -lboost_unit_test_framework -fprofile-arcs -ftest-coverage -o exception-test except.cpp
 g++ -std=c++1z -I${ROOT_DIR}/include -I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib -lboost_unit_test_framework -lboost_filesystem -lssl -lcrypto -ldl -lyaml-cpp -fprofile-arcs -ftest-coverage -DPOAC_VERSION=\"0.2.0\" -DPOAC_PROJECT_ROOT=\"${ROOT_DIR}\" -o infer-test infer.cpp
 
-
+cd ../../../
 coveralls --gcov-options '\-lp' -t ${COVERALLS_TOKEN}
