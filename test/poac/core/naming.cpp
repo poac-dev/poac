480dd0a5ea43cdd8d517dd058946b2e6515206f0
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-05-31 05:43:18 +0900

    Add test

--- test/poac/core/naming.cpp
+++ test/poac/core/naming.cpp
@@ -1,53 +1,74 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+
 #include <yaml-cpp/yaml.h>
 #include <poac/core/naming.hpp>
+#include <poac/core/except.hpp>
 
 
 // std::string basename(const std::string& name)
-BOOST_AUTO_TEST_CASE( poac_core_naming_test1 )
+BOOST_AUTO_TEST_CASE( poac_core_naming_basename_test )
 {
     using namespace poac::core;
-    BOOST_TEST( naming::basename("username/repository") == "repository" );
-    BOOST_TEST( naming::basename("repository") == "repository" );
+    BOOST_TEST( naming::basename("user/repo") == "repo" );
+    BOOST_TEST( naming::basename("repo") == "repo" );
 }
 
-// std::string github_conv_pkgname(const std::string& name, const std::string& tag)
-BOOST_AUTO_TEST_CASE( poac_core_naming_test2 )
+// std::string slash_to_hyphen(std::string name)
+BOOST_AUTO_TEST_CASE( poac_core_naming_slash_to_hyphen_test )
 {
     using namespace poac::core;
-    BOOST_TEST( naming::github_conv_pkgname("opencv/opencv", "3.4.2") == "opencv-3.4.2" );
-    BOOST_TEST( naming::github_conv_pkgname("curl/curl", "7_61_0") == "curl-7.61.0" );
-    BOOST_TEST( naming::github_conv_pkgname("matken11235/logger_slack_backend", "v0.1.0") == "logger_slack_backend-0.1.0" );
-    BOOST_TEST( naming::github_conv_pkgname("curl/curl", "curl-7_61_0") == "curl-7_61_0" );
-    // TODO: not yet 5
-    //BOOST_TEST( naming::github_conv_pkgname("boostorg/optional", "boost-1.67.0") == "boost-optional-1.67.0" );
+    BOOST_TEST( naming::slash_to_hyphen("user/repo") == "user-repo" );
+    BOOST_TEST( naming::slash_to_hyphen("repo") == "repo" );
 }
 
-// std::string cache_to_current(const std::string& pkgname)
-BOOST_AUTO_TEST_CASE( poac_core_naming_test3 )
+// std::string hyphen_to_slash(std::string name)
+BOOST_AUTO_TEST_CASE( poac_core_naming_hyphen_to_slash_test )
+{
+    using namespace poac::core;
+    BOOST_TEST( naming::hyphen_to_slash("user-repo") == "user/repo" );
+    BOOST_TEST( naming::hyphen_to_slash("repo") == "repo" );
+}
+
+// std::string to_cache_github(const std::string& name, const std::string& tag)
+BOOST_AUTO_TEST_CASE( poac_core_naming_to_cache_github_test )
+{
+    using namespace poac::core;
+    BOOST_TEST( naming::to_cache_github("opencv/opencv", "3.4.2") == "opencv-3.4.2" );
+    BOOST_TEST( naming::to_cache_github("curl/curl", "7_61_0") == "curl-7.61.0" );
+    BOOST_TEST( naming::to_cache_github("matken11235/logger_slack_backend", "v0.1.0") == "logger_slack_backend-0.1.0" );
+    BOOST_TEST( naming::to_cache_github("curl/curl", "curl-7_61_0") == "curl-7_61_0" );
+}
+
+// std::string to_cache_poac(const std::string& name, const std::string& ver)
+BOOST_AUTO_TEST_CASE( poac_core_naming_to_cache_poac_test )
+{
+    using namespace poac::core;
+    BOOST_TEST( naming::to_cache_poac("boost/optional", "1.66.0") == "boost-optional-1.66.0" );
+}
+
+// std::string cache_to_current(const std::string& name)
+BOOST_AUTO_TEST_CASE( poac_core_naming_cache_to_current_test )
 {
     using namespace poac::core;
     BOOST_TEST( naming::cache_to_current("opencv-3.4.2") == "opencv" );
     BOOST_TEST( naming::cache_to_current("opencv") == "opencv" );
 }
 
-// std::string poac_conv_pkgname(const std::string& name, const std::string& tag)
-BOOST_AUTO_TEST_CASE( poac_core_naming_test4 )
+// std::string to_cache(const std::string& src, const std::string& name, const std::string& version)
+BOOST_AUTO_TEST_CASE( poac_core_naming_to_cache_test )
 {
     using namespace poac::core;
-    BOOST_TEST( naming::poac_conv_pkgname("opencv", "3.4.2") == "opencv-3.4.2" );
-}
+    BOOST_TEST( naming::to_cache("poac", "boost/optional", "1.66.0") == "boost-optional-1.66.0" );
 
-// std::string get_source(const YAML::Node& node)
-BOOST_AUTO_TEST_CASE( poac_core_naming_test5 )
-{
-    using namespace poac::core;
+    BOOST_TEST( naming::to_cache("github", "opencv/opencv", "3.4.2") == "opencv-3.4.2" );
+    BOOST_TEST( naming::to_cache("github", "curl/curl", "7_61_0") == "curl-7.61.0" );
+    BOOST_TEST( naming::to_cache("github", "matken11235/logger_slack_backend", "v0.1.0") == "logger_slack_backend-0.1.0" );
+    BOOST_TEST( naming::to_cache("github", "curl/curl", "curl-7_61_0") == "curl-7_61_0" );
 
-    const YAML::Node node = YAML::Load("src: github");
-    BOOST_TEST( naming::get_source(node) == "github" );
-
-    const YAML::Node node2 = YAML::Load("");
-    BOOST_TEST( naming::get_source(node2) == "poac" );
+    BOOST_CHECK_THROW(
+            naming::to_cache("unknown", "", ""),
+            except::error
+    );
 }
