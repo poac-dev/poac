f3740d4ab1e781cfe4e2112c7c09186fe83a0dca
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-06-03 07:06:34 +0900

    Improve include/poac/core/stroite/utils/absorb.hpp

--- include/poac/core/stroite/utils/absorb.hpp
+++ include/poac/core/stroite/utils/absorb.hpp
@@ -1,13 +1,14 @@
 #ifndef POAC_CORE_STROITE_UTILS_ABSORB_HPP
 #define POAC_CORE_STROITE_UTILS_ABSORB_HPP
 
+#include <boost/predef.h>
 
 namespace poac::core::stroite::utils::absorb {
-#ifdef _WIN32
+#if BOOST_OS_WINDOWS
     const std::string binary_extension = ".exe";
     const std::string dynamic_lib_extension = ".dll";
     const std::string dynamic_lib_option = "-shared -fPIC";
-#elif __APPLE__
+#elif BOOST_OS_MACOS
     const std::string binary_extension = "";
     const std::string dynamic_lib_extension = ".dylib";
     const std::string dynamic_lib_option = "-dynamiclib";
