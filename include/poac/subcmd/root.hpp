d758df91831262950b7098148c63e65f15acfec3
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-06-03 07:40:38 +0900

    Resolve #125

--- include/poac/subcmd/root.hpp
+++ include/poac/subcmd/root.hpp
@@ -3,8 +3,23 @@
 
 #include <iostream>
 #include <string>
-#include <type_traits>
 #include <cstdlib>
+
+#include <boost/predef.h>
+#include <boost/filesystem.hpp>
+
+#if BOOST_OS_WINDOWS
+#   include <libloaderapi.h>
+#elif BOOST_OS_MACOS
+#   include <mach-o/dyld.h>
+#elif BOOST_OS_SOLARIS
+#   include <stdlib.h>
+#elif BOOST_OS_BSD_FREE
+#   include <sys/sysctl.h>
+#endif
+
+#include "../core/except.hpp"
+
 
 namespace poac::subcmd {
     struct root {
@@ -14,10 +29,63 @@ namespace poac::subcmd {
         static std::string options() {
             return "<Nothing>";
         }
-        template<typename VS>
+
+        // Reference: https://stackoverflow.com/a/1024937
+        template <typename VS>
         int operator()([[maybe_unused]] VS&& vs) {
-            std::cout << POAC_PROJECT_ROOT << std::endl;
+            namespace fs = boost::filesystem;
+            constexpr int PATH_SIZE = 1024;
+            boost::system::error_code ec;
+
+#if BOOST_OS_WINDOWS
+            char path[PATH_SIZE];
+            if (GetModuleFileNameA(NULL, path, sizeof(path) / sizeof(path[0])) != 0) {
+                boost::filesystem::path p{ path };
+                std::cout << p.parent_path().string() << std::endl;
+                return EXIT_SUCCESS;
+            }
+
+#elif BOOST_OS_MACOS
+            char path[PATH_SIZE];
+            uint32_t size = sizeof(path);
+            if (_NSGetExecutablePath(path, &size) == 0) {
+                std::cout << fs::read_symlink(path, ec).parent_path().string() << std::endl;
+                return EXIT_SUCCESS;
+            }
+
+#elif BOOST_OS_LINUX
+            std::cout << fs::read_symlink("/proc/self/exe", ec).parent_path().string() << std::endl;
             return EXIT_SUCCESS;
+
+#elif BOOST_OS_SOLARIS
+            if (const char* path = getexecname(); path != 0) {
+                boost::filesystem::path p{ path };
+                std::cout << p.parent_path().string() << std::endl;
+                return EXIT_SUCCESS;
+            }
+
+#elif BOOST_OS_BSD_FREE
+            // Reference: https://reviews.llvm.org/file/data/yrq2xrvzohnbgsj7ougv/PHID-FILE-srzn7usmowtvqug74hnb/D5693.diff
+            char path[MAXPATHLEN];
+            size_t len = sizeof(path);
+            int name[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME };
+            name[3] = getpid();
+            if (sysctl(name, 4, path, &len, NULL, 0) == 0) {
+                boost::filesystem::path p{ path };
+                std::cout << p.parent_path().string() << std::endl;
+                return EXIT_SUCCESS;
+            }
+
+#elif BOOST_OS_BSD_DRAGONFLY
+            std::cout << fs::read_symlink("/proc/curproc/file", ec).parent_path().string() << std::endl;
+            return EXIT_SUCCESS;
+
+#elif BOOST_OS_BSD_NET
+            std::cout << fs::read_symlink("/proc/curproc/exe", ec).parent_path().string() << std::endl;
+            return EXIT_SUCCESS;
+
+#endif
+            throw core::except::error("Could not get root installation directory");
         }
     };
 } // end namespace
