9b62e7533f0b505ecd15bdc42d6fc2d8b0bf4676
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-06-23 21:41:02 +0900

    Improve tests/poac/core/except.cpp

--- tests/poac/core/except.cpp
+++ tests/poac/core/except.cpp
@@ -1,12 +1,13 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
-#include <poac/core/except.hpp>
 
+#include <string_view>
+#include <poac/core/except.hpp>
 
 BOOST_AUTO_TEST_CASE( poac_core_except_test1 )
 {
-    using namespace poac::core::except;
+    using poac::core::except::invalid_first_arg;
 
     const std::string msg = "hoge";
     try {
@@ -19,7 +20,7 @@ BOOST_AUTO_TEST_CASE( poac_core_except_test1 )
 
 BOOST_AUTO_TEST_CASE( poac_core_except_test2 )
 {
-    using namespace poac::core::except;
+    using poac::core::except::invalid_second_arg;
 
     const std::string msg = "hoge";
     try {
@@ -32,7 +33,7 @@ BOOST_AUTO_TEST_CASE( poac_core_except_test2 )
 
 BOOST_AUTO_TEST_CASE( poac_core_except_test3 )
 {
-    using namespace poac::core::except;
+    using poac::core::except::error;
 
     const std::string msg = "hoge";
     try {
@@ -45,13 +46,15 @@ BOOST_AUTO_TEST_CASE( poac_core_except_test3 )
 
 BOOST_AUTO_TEST_CASE( poac_core_except_test4 )
 {
-    using namespace poac::core::except;
+    using poac::core::except::error;
 
-    const std::string msg = "hoge";
     try {
-        throw warn(msg);
+        std::string_view msg1 = "ge,h";
+        const std::string msg2 = "oge: ";
+        const unsigned int msg3 = 10;
+        throw error("ho", msg1, msg2, msg3);
     }
-    catch (const warn& e) {
-        BOOST_TEST(e.what() == msg);
+    catch (const error& e) {
+        BOOST_TEST(e.what() == "hoge,hoge: 10");
     }
 }
