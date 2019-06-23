2e570e04122c4fe0a96bb6c24f8916c92842b20c
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-06-24 03:29:46 +0900

    Fix subcmd/publish.hpp

--- include/poac/subcmd/publish.hpp
+++ include/poac/subcmd/publish.hpp
@@ -31,114 +31,114 @@
 
 namespace poac::subcmd {
     namespace _publish {
-        struct PackageInfo {
-            std::string name;
-            core::deper::semver::Version version;
-            std::optional<std::string> description;
-            std::uint16_t cpp_version;
-            std::optional<std::string> license;
-            std::string package_type;
-        };
-
-        int
-        do_register() {
-
-        }
-
-        template <typename VS>
-        int
-        confirm(const VS& argv) {
-            const bool yes = util::argparse::use(argv, "-y", "--yes");
-            if (!yes) {
-                std::cout << "Are you sure publish this package? [Y/n] ";
-                if (!io::cli::yes_or_no()) {
-                    std::cout << "canceled." << std::endl;
-                    return EXIT_FAILURE;
-                }
-            }
-            return EXIT_SUCCESS;
-        }
-
-        void
-        summarize(const PackageInfo& package_info) {
-            namespace pretty = util::pretty;
-            std::cout << "Summary:"
-                      << "\n  Name: " << package_info.name
-                      << "\n  Version: " << package_info.version.get_version() // TODO: GET /repos/:owner/:repo/releases/latest tag_name -> SemVerに即している必要がある．
-                      << "\n  Description: " << pretty::clip_string(package_info.description.value_or("null"), 50) // TODO: GET /repos/:owner/:repo, なければ，nullとなる．-> 長い場合，prettyする？50くらい
-                      << "\n  C++ Version (minimum required version): " << package_info.cpp_version
-                      << "\n  License: " << package_info.license.value_or("null") // TODO: /repos/poacpm/poac/license\?ref\=0.2.1 -> license -> name
-                      << "\n  Package Type: " << package_info.package_type
-                      << "\n\n" << std::endl;
-        }
-
-        std::string
-        get_package_type() {
-
-        }
-
-        std::optional<std::string>
-        get_license(const std::string& full_name) {
-
-        }
-
-        std::uint16_t
-        get_cpp_version() {
-            return io::yaml::load_config("cpp_version").as<std::uint16_t>();
-        }
-
-        std::optional<std::string>
-        get_description(const std::string& full_name) {
-
-        }
-
-        core::deper::semver::Version
-        get_version(const std::string& full_name) {
-            // https://developer.github.com/v3/repos/releases/#get-the-latest-release
-            const io::net::requests req{ GITHUB_API_HOST };
-            const auto res = req.get(GITHUB_REPOS_API + full_name + "/releases/latest");
-
-            std::stringstream ss;
-            ss << res.data();
-
-            boost::property_tree::ptree pt;
-            boost::property_tree::json_parser::read_json(ss, pt);
-        }
-
-        std::string_view
-        extract_full_name(std::string_view repository) {
-            auto first = repository.find("https://github.com/");
-            auto last = repository.find(".git", first);
-            return repository.substr(first, last - first);
-        }
-
-        std::string
-        get_name() {
-            const std::string repository = io::yaml::load_config("repository").as<std::string>();
-            std::string_view full_name = extract_full_name(repository);
-            return std::string(full_name);
-        }
-
-        PackageInfo
-        gather_package_info() {
-            const std::string full_name = get_name();
-            return PackageInfo{
-                    full_name,
-                    get_version(full_name),
-                    get_description(full_name),
-                    get_cpp_version(),
-                    get_license(full_name),
-                    get_package_type()
-            };
-        }
-
-        PackageInfo
-        report_publish_start() { // TODO: optionalで返さなくてOK????
-            std::cout << "Verifying your package ...\n" << std::endl;
-            const PackageInfo package_info = gather_package_info();
-            summarize(package_info);
-            return package_info;
-        }
+//        struct PackageInfo {
+//            std::string name;
+//            core::deper::semver::Version version;
+//            std::optional<std::string> description;
+//            std::uint16_t cpp_version;
+//            std::optional<std::string> license;
+//            std::string package_type;
+//        };
+//
+//        int
+//        do_register() {
+//
+//        }
+//
+//        template <typename VS>
+//        int
+//        confirm(const VS& argv) {
+//            const bool yes = util::argparse::use(argv, "-y", "--yes");
+//            if (!yes) {
+//                std::cout << "Are you sure publish this package? [Y/n] ";
+//                if (!io::cli::yes_or_no()) {
+//                    std::cout << "canceled." << std::endl;
+//                    return EXIT_FAILURE;
+//                }
+//            }
+//            return EXIT_SUCCESS;
+//        }
+//
+//        void
+//        summarize(const PackageInfo& package_info) {
+//            namespace pretty = util::pretty;
+//            std::cout << "Summary:"
+//                      << "\n  Name: " << package_info.name
+//                      << "\n  Version: " << package_info.version.get_version() // TODO: GET /repos/:owner/:repo/releases/latest tag_name -> SemVerに即している必要がある．
+//                      << "\n  Description: " << pretty::clip_string(package_info.description.value_or("null"), 50) // TODO: GET /repos/:owner/:repo, なければ，nullとなる．-> 長い場合，prettyする？50くらい
+//                      << "\n  C++ Version (minimum required version): " << package_info.cpp_version
+//                      << "\n  License: " << package_info.license.value_or("null") // TODO: /repos/poacpm/poac/license\?ref\=0.2.1 -> license -> name
+//                      << "\n  Package Type: " << package_info.package_type
+//                      << "\n\n" << std::endl;
+//        }
+//
+//        std::string
+//        get_package_type() {
+//
+//        }
+//
+//        std::optional<std::string>
+//        get_license(const std::string& full_name) {
+//
+//        }
+//
+//        std::uint16_t
+//        get_cpp_version() {
+//            return io::yaml::load_config("cpp_version").as<std::uint16_t>();
+//        }
+//
+//        std::optional<std::string>
+//        get_description(const std::string& full_name) {
+//
+//        }
+//
+//        core::deper::semver::Version
+//        get_version(const std::string& full_name) {
+//            // https://developer.github.com/v3/repos/releases/#get-the-latest-release
+//            const io::net::requests req{ GITHUB_API_HOST };
+//            const auto res = req.get(GITHUB_REPOS_API + full_name + "/releases/latest");
+//
+//            std::stringstream ss;
+//            ss << res.data();
+//
+//            boost::property_tree::ptree pt;
+//            boost::property_tree::json_parser::read_json(ss, pt);
+//        }
+//
+//        std::string_view
+//        extract_full_name(std::string_view repository) {
+//            auto first = repository.find("https://github.com/");
+//            auto last = repository.find(".git", first);
+//            return repository.substr(first, last - first);
+//        }
+//
+//        std::string
+//        get_name() {
+//            const std::string repository = io::yaml::load_config("repository").as<std::string>();
+//            std::string_view full_name = extract_full_name(repository);
+//            return std::string(full_name);
+//        }
+//
+//        PackageInfo
+//        gather_package_info() {
+//            const std::string full_name = get_name();
+//            return PackageInfo{
+//                    full_name,
+//                    get_version(full_name),
+//                    get_description(full_name),
+//                    get_cpp_version(),
+//                    get_license(full_name),
+//                    get_package_type()
+//            };
+//        }
+//
+//        PackageInfo
+//        report_publish_start() { // TODO: optionalで返さなくてOK????
+//            std::cout << "Verifying your package ...\n" << std::endl;
+//            const PackageInfo package_info = gather_package_info();
+//            summarize(package_info);
+//            return package_info;
+//        }
 
         template <typename VS>
         void
