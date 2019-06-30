49c292b676d147b02951bf9bc1824d9f19411307
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-06-30 14:15:50 +0900

    Fix include/poac/util/semver.hpp

--- include/poac/util/semver.hpp
+++ include/poac/util/semver.hpp
@@ -1,12 +1,10 @@
 #ifndef SEMVER_HPP
 #define SEMVER_HPP
 
-#include "semver/alias.hpp"
 #include "semver/comparison.hpp"
 #include "semver/interval.hpp"
 #include "semver/io.hpp"
 #include "semver/lexer.hpp"
 #include "semver/version.hpp"
-#include "semver/version_traits.hpp"
 
 #endif // !SEMVER_HPP
