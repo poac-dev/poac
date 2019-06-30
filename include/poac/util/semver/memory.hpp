8da4e5abd16c65faa8ea474578bf4575ae7634bf
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-06-30 21:36:51 +0900

    Move include/poac/util/semver/vector/pointer_traits.hpp -> include/poac/util/semver/memory/pointer_traits.hpp

---
+++ include/poac/util/semver/memory.hpp
@@ -0,0 +1,6 @@
+#ifndef SEMVER_MEMORY_HPP
+#define SEMVER_MEMORY_HPP
+
+#include "memory/pointer_traits.hpp"
+
+#endif // !SEMVER_MEMORY_HPP
--- include/poac/util/semver/vector.hpp
+++ include/poac/util/semver/vector.hpp
@@ -1,6 +1,5 @@
 #ifndef SEMVER_VECTOR_HPP
 #define SEMVER_VECTOR_HPP
 
-#include "vector/pointer_traits.hpp"
 
 #endif // !SEMVER_VECTOR_HPP
