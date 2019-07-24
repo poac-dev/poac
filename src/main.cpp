0e664f36269fc0ae5ef7fd82442869198dfbdf29
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-07-25 06:26:13 +0900

    Improve src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,32 +9,28 @@
 #include <poac/poac.hpp>
 
 int handle(std::string_view cmd, std::vector<std::string>&& args) {
-    namespace cli = poac::core::cli;
-    namespace except = poac::core::except;
-    namespace term = poac::io::term;
-
     try {
-        const auto error = cli::exec(std::move(cmd), std::move(args));
+        const auto error = poac::core::cli::exec(std::move(cmd), std::move(args));
         if (!error) {
             return EXIT_SUCCESS;
         }
-
-        if (std::holds_alternative<except::Error::InvalidSecondArg>(error->state)) {
+        if (std::holds_alternative<poac::core::except::Error::InvalidSecondArg>(error->state)) {
             handle("help", std::vector<std::string>{error->what()});
-        }
-        else {
-            std::cerr << term::error << error->what() << std::endl;
+        } else {
+            std::cerr << poac::io::term::error << error->what() << std::endl;
         }
         return EXIT_FAILURE;
-    } catch (const except::error& e) {
-        std::cerr << term::error << e.what() << std::endl;
+    } catch (const poac::core::except::error& e) {
+        std::cerr << poac::io::term::error << e.what() << std::endl;
         return EXIT_FAILURE;
-    } catch (const YAML::BadConversion& e) {
-        std::cout << term::error << "poac.yml " << e.what()
-                  << std::endl;
+    } catch (const YAML::Exception& e) {
+        std::cout << poac::io::term::error << "poac.yml " << e.what() << std::endl;
         return EXIT_SUCCESS;
+    } catch (const toml::exception& e) {
+        std::cout << poac::io::term::error << e.what() << std::endl;
+        return EXIT_FAILURE;
     } catch (...) {
-        std::cerr << term::error << "Unexpected error" << std::endl;
+        std::cerr << poac::io::term::error << "Unexpected error" << std::endl;
         return EXIT_FAILURE;
     }
 }
