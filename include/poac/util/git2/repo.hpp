276e1d5f545cf9c84acc39115c473d6c5d662ef5
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-07-28 06:29:09 +0900

    Improve include/poac/util/git2/repo.hpp

--- include/poac/util/git2/repo.hpp
+++ include/poac/util/git2/repo.hpp
@@ -2,15 +2,30 @@
 #define GIT2_REPO_HPP
 
 #include <string>
+#include <optional>
 
 #include <git2.h>
 
 namespace git2::repository {
-    int init(const std::string& path) {
+    /**
+     * Creates a new Git repository in the given folder.
+     *
+     * TODO:
+     *	- Reinit the repository
+     *
+     * @param path the path to the repository
+     * @param is_bare if true, a Git repository without a working directory is
+     *		created at the pointed path. If false, provided path will be
+     *		considered as the working directory into which the .git directory
+     *		will be created.
+     *
+     * @return 0 or an error code
+     */
+    int init(const std::string& path, const bool is_bare = false) {
         git_libgit2_init();
 
         git_repository* repo = nullptr;
-        if (int error = git_repository_init(&repo, path.c_str(), false); error != 0) {
+        if (int error = git_repository_init(&repo, path.c_str(), is_bare); error != 0) {
             return error;
         }
         git_repository_free(repo);
