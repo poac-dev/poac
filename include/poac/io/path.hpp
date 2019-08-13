914fb3e99d79f7fc2290fc8e25c41b3766a9294b
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-08-14 04:26:36 +0900

    Improve include/poac/io/path.hpp

--- include/poac/io/path.hpp
+++ include/poac/io/path.hpp
@@ -194,11 +194,11 @@ namespace poac::io::path {
 #endif
 
     std::string
-    time_to_string(const std::time_t time) {
+    time_to_string(const std::time_t& time) {
         return std::to_string(time);
     }
     std::string
-    time_to_string(const std::chrono::system_clock::time_point time) {
+    time_to_string(const std::chrono::system_clock::time_point& time) {
         return time_to_string(std::chrono::system_clock::to_time_t(time));
     }
 
