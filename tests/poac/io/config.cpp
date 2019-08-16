bd2e344c4c2415a82c280b37924a2a7d6c05a8e4
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-08-17 04:14:30 +0900

    Improve tests/poac/io/config.cpp

--- tests/poac/io/config.cpp
+++ tests/poac/io/config.cpp
@@ -1,42 +1,26 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
-#include <boost/filesystem.hpp>
 
 #include <string>
-#include <vector>
-#include <map>
-#include <fstream>
 
 #include <poac/io/config.hpp>
 #include <poac/core/except.hpp>
 
-// std::optional<T> get(const YAML::Node& node)
-BOOST_AUTO_TEST_CASE( poac_io_config_get_test )
+// [[noreturn]] inline void rethrow_bad_cast(const std::string& what)
+BOOST_AUTO_TEST_CASE( poac_io_config_detail_rethrow_bad_cast_test )
 {
-//    using poac::io::config::detail::get;
-//
-//    // std::optional<T> get(const YAML::Node& node)
-//    {
-//        YAML::Node node = YAML::Load("bar");
-//        BOOST_CHECK( get<std::string>(node).value() == "bar" );
-//    }
-
-//    const auto c = config.get();
-//
-////        std::cout << "fds" << std::endl;
-////        for (const auto& op : hoge.options.value()) {
-////            std::cout << op << std::endl;
-////        }
-////        std::cout << "fds" << std::endl;
-////
-//    std::cout << "dev:" << std::endl;
-//    for (const auto& op : c->profile->dev->options.value()) {
-//        std::cout << op << std::endl;
-//    }
-//    std::cout << "bench:" << std::endl;
-//    for (const auto& op : c->profile->bench->options.value()) {
-//        std::cout << op << std::endl;
-//    }
-////        std::cout << std::endl <<  c->profile->dev.compiler.value() << std::endl;
+    using poac::io::config::detail::rethrow_bad_cast;
+    try {
+        rethrow_bad_cast("[error] toml::value bad_cast to string\n"
+                         " --> poac.toml\n"
+                         " 2 | name = []\n"
+                         "   |        ~~ the actual type is array");
+    } catch (const toml::type_error& e) {
+        BOOST_CHECK( std::string(e.what()) ==
+            "[error] value type should be string\n"
+            " --> poac.toml\n"
+            " 2 | name = []\n"
+            "   |        ~~ the actual type is array" );
+    }
 }
