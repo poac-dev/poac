6c2d8eceb49b9eb22dbde4b0006f8d0eba1f1acc
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-08-17 09:15:31 +0900

    Move tests/poac/core/* to tests/

--- test.sh
+++ test.sh
@@ -12,22 +12,6 @@ execute() {
 }
 
 main() {
-  pushd ./tests/poac
-  pushd ./core
-    pushd ./builder
-      execute g++ ${BASE_OPT} -lboost_filesystem -o standard-test standard.cpp && { ./standard-test; rm -f ./standard-test; }
-    popd
-    pushd ./resolver
-      execute g++ ${BASE_OPT} ${REQUIRE_OPENSSL} ${REQUIRE_POAC_VARIABLES} -lboost_filesystem -o resolve-test resolve.cpp && { ./resolve-test; rm -f ./resolve-test; }
-      execute g++ ${BASE_OPT} -o sat-test sat.cpp && { ./sat-test; rm -f ./sat-test; }
-    popd
-    execute g++ ${BASE_OPT} ${REQUIRE_OPENSSL} ${REQUIRE_POAC_VARIABLES} -lboost_filesystem -lgit2 -o cli-test cli.cpp && { ./cli-test; rm -f ./cli-test; }
-    execute g++ ${BASE_OPT} -o exception-test except.cpp && { ./exception-test; rm -f ./exception-test; }
-    execute g++ ${BASE_OPT} -lboost_filesystem -o name-test name.cpp && { ./name-test; rm -f ./name-test; }
-    execute g++ ${BASE_OPT} -lgit2 -o project-test project.cpp && { ./project-test; rm -f ./project-test; }
-  popd
-  popd
-
   pushd ./tests
   pushd ./semver
     pushd ./parser
@@ -39,12 +23,19 @@ main() {
     execute g++ ${BASE_OPT} -o version-test version.cpp && { ./version-test; rm -f ./version-test; }
   popd
   execute g++ ${BASE_OPT} -o argparse-test argparse.cpp && { ./argparse-test; rm -f ./argparse-test; }
+  execute g++ ${BASE_OPT} ${REQUIRE_OPENSSL} ${REQUIRE_POAC_VARIABLES} -lboost_filesystem -lgit2 -o cli-test cli.cpp && { ./cli-test; rm -f ./cli-test; }
   execute g++ ${BASE_OPT} -lboost_filesystem -o config-test config.cpp && { ./config-test; rm -f ./config-test; }
+  execute g++ ${BASE_OPT} -o exception-test except.cpp && { ./exception-test; rm -f ./exception-test; }
   execute g++ ${BASE_OPT} -o misc-test misc.cpp && { ./misc-test; rm -f ./misc-test; }
+  execute g++ ${BASE_OPT} -lboost_filesystem -o name-test name.cpp && { ./name-test; rm -f ./name-test; }
   execute g++ ${BASE_OPT} -lboost_filesystem -o path-test path.cpp && { ./path-test; rm -f ./path-test; }
   execute g++ ${BASE_OPT} -o pretty-test pretty.cpp && { ./pretty-test; rm -f ./pretty-test; }
+  execute g++ ${BASE_OPT} -lgit2 -o project-test project.cpp && { ./project-test; rm -f ./project-test; }
   execute g++ ${BASE_OPT} ${REQUIRE_OPENSSL} ${REQUIRE_POAC_VARIABLES} -lboost_filesystem -lgit2 -o publish-test publish.cpp && { ./publish-test; rm -f ./publish-test; }
+  execute g++ ${BASE_OPT} ${REQUIRE_OPENSSL} ${REQUIRE_POAC_VARIABLES} -lboost_filesystem -o resolve-test resolve.cpp && { ./resolve-test; rm -f ./resolve-test; }
+  execute g++ ${BASE_OPT} -o sat-test sat.cpp && { ./sat-test; rm -f ./sat-test; }
   execute g++ ${BASE_OPT} -o shell-test shell.cpp && { ./shell-test; rm -f ./shell-test; }
+  execute g++ ${BASE_OPT} -lboost_filesystem -o standard-test standard.cpp && { ./standard-test; rm -f ./standard-test; }
   execute g++ ${BASE_OPT} -o term-test term.cpp && { ./term-test; rm -f ./term-test; }
   execute g++ ${BASE_OPT} -o types-test types.cpp && { ./types-test; rm -f ./types-test; }
   popd
