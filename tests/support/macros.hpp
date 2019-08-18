fe70a71397b8c9707b8715b505184269f3fc9fc7
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-08-19 03:02:36 +0900

    Improve tests/support/macros.hpp

--- tests/support/macros.hpp
+++ tests/support/macros.hpp
@@ -7,14 +7,14 @@
 // M - message
 // S - statement
 // E - exception
-#define BOOST_CHECK_THROW_MSG( S, E, M )              \
-do {                                                  \
-    BOOST_CHECK_THROW( S, E );                        \
-    try {                                             \
-        S;                                            \
-    } catch(const E& e) {                             \
-        BOOST_CHECK( std::string(e.what()) == M);     \
-    }                                                 \
-} while( boost::test_tools::tt_detail::dummy_cond() ) \
+#define BOOST_CHECK_THROW_MSG( S, E, M )               \
+do {                                                   \
+    BOOST_CHECK_THROW( S, E );                         \
+    try {                                              \
+        S;                                             \
+    } catch (const E& e) {                             \
+        BOOST_CHECK( std::string(e.what()) == M );     \
+    }                                                  \
+} while ( boost::test_tools::tt_detail::dummy_cond() ) \
 
 #endif // !POAC_TESTS_SUPPORT_MACROS_HPP
