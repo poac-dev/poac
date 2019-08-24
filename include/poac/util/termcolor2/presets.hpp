c3b2ce29df8a817e815686c544e3794ad724abc4
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-08-24 16:05:14 +0900

    Improve include/poac/util/termcolor2/presets.hpp

--- include/poac/util/termcolor2/presets.hpp
+++ include/poac/util/termcolor2/presets.hpp
@@ -9,13 +9,13 @@ namespace termcolor2 {
     // Foreground manipulators
     //
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto gray_v = make_string("\x1b[30m");
     template <>
@@ -29,13 +29,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto gray = gray_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto red_v = make_string("\x1b[31m");
     template <>
@@ -49,13 +49,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto red = red_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto green_v = make_string("\x1b[32m");
     template <>
@@ -69,13 +69,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto green = green_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto yellow_v = make_string("\x1b[33m");
     template <>
@@ -89,13 +89,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto yellow = yellow_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto blue_v = make_string("\x1b[34m");
     template <>
@@ -109,13 +109,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto blue = blue_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto magenta_v = make_string("\x1b[35m");
     template <>
@@ -129,13 +129,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto magenta = magenta_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto cyan_v = make_string("\x1b[36m");
     template <>
@@ -149,13 +149,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto cyan = cyan_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto white_v = make_string("\x1b[37m");
     template <>
@@ -172,13 +172,13 @@ namespace termcolor2 {
     // Background manipulators
     //
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_gray_v = make_string("\x1b[40m");
     template <>
@@ -192,13 +192,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_gray = on_gray_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_red_v = make_string("\x1b[41m");
     template <>
@@ -212,13 +212,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_red = on_red_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_green_v = make_string("\x1b[42m");
     template <>
@@ -232,13 +232,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_green = on_green_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_yellow_v = make_string("\x1b[43m");
     template <>
@@ -252,13 +252,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_yellow = on_yellow_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_blue_v = make_string("\x1b[44m");
     template <>
@@ -272,13 +272,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_blue = on_blue_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_magenta_v = make_string("\x1b[45m");
     template <>
@@ -292,13 +292,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_magenta = on_magenta_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_cyan_v = make_string("\x1b[46m");
     template <>
@@ -312,13 +312,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_cyan = on_cyan_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto on_white_v = make_string("\x1b[47m");
     template <>
@@ -335,13 +335,13 @@ namespace termcolor2 {
     // Attribute manipulators
     //
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto bold_v = make_string("\x1b[1m");
     template <>
@@ -355,13 +355,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto bold = bold_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto dark_v = make_string("\x1b[2m");
     template <>
@@ -375,13 +375,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto dark = dark_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto underline_v = make_string("\x1b[4m");
     template <>
@@ -395,13 +395,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto underline = underline_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto blink_v = make_string("\x1b[5m");
     template <>
@@ -415,13 +415,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto blink = blink_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto reverse_v = make_string("\x1b[7m");
     template <>
@@ -435,13 +435,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto reverse = reverse_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto concealed_v = make_string("\x1b[8m");
     template <>
@@ -455,13 +455,13 @@ namespace termcolor2 {
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto concealed = concealed_v<char>;
 
     template <typename CharT = char,
-        std::enable_if_t<
+        typename std::enable_if<
             std::is_same<CharT, char>::value     ||
             std::is_same<CharT, char16_t>::value ||
             std::is_same<CharT, char32_t>::value ||
             std::is_same<CharT, wchar_t>::value
             , std::nullptr_t
-        > = nullptr
+        >::type = nullptr
     >
     TERMCOLOR2_INLINE_VARIABLES_AFTER_CXX14 constexpr auto reset_v = make_string("\x1b[0m");
     template <>
