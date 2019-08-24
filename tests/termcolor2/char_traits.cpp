7fe2dc1ecbe5fe568505987656cef37cbfb53230
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-08-24 20:36:46 +0900

    Improve tests/termcolor2/char_traits.cpp

--- tests/termcolor2/char_traits.cpp
+++ tests/termcolor2/char_traits.cpp
@@ -10,15 +10,15 @@ constexpr char assign() {
 
 BOOST_AUTO_TEST_CASE( termcolor2_char_traits_assign_test )
 {
-    static_assert( assign() == 'b' );
+    static_assert( assign() == 'b', "" );
 }
 
 BOOST_AUTO_TEST_CASE( termcolor2_char_traits_compare_test )
 {
-    static_assert( termcolor2::char_traits<char>::compare("foo", "foo", 3) == 0 );
-    static_assert( termcolor2::char_traits<char>::compare("aoo", "foo", 3) == -1 );
-    static_assert( termcolor2::char_traits<char>::compare("foo", "aoo", 3) == 1 );
-    static_assert( termcolor2::char_traits<char>::compare("foa", "foz", 3) == -1 );
-    static_assert( termcolor2::char_traits<char>::compare("foz", "foa", 3) == 1 );
-    static_assert( termcolor2::char_traits<char>::compare("foo", "foobar", 3) == 0 );
+    static_assert( termcolor2::char_traits<char>::compare("foo", "foo", 3) == 0, "" );
+    static_assert( termcolor2::char_traits<char>::compare("aoo", "foo", 3) == -1, "" );
+    static_assert( termcolor2::char_traits<char>::compare("foo", "aoo", 3) == 1, "" );
+    static_assert( termcolor2::char_traits<char>::compare("foa", "foz", 3) == -1, "" );
+    static_assert( termcolor2::char_traits<char>::compare("foz", "foa", 3) == 1, "" );
+    static_assert( termcolor2::char_traits<char>::compare("foo", "foobar", 3) == 0, "" );
 }
