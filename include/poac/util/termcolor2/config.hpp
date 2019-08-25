e972ae5d527005348382e426687dab5232957057
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-08-25 14:43:43 +0900

    Improve include/poac/util/termcolor2/config.hpp

--- include/poac/util/termcolor2/config.hpp
+++ include/poac/util/termcolor2/config.hpp
@@ -44,7 +44,6 @@
 #ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
 #  ifdef _MSC_VER
 //   Does not implement constexpr color literals for MSVC.
-#    define COLOR_LITERALS_CONSTEXPR const
 #    undef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
 #    ifndef TERMCOLOR2_NO_WARNING
 #      pragma message ( "You cannot enable gnu-string-literal-operator-template on MSVC." )
@@ -53,12 +52,7 @@
 #    warning "gnu-string-literal-operator-template is enabled." \
              "You will see a warning associated with it." \
              "If you want to hide warning, please pass -Wno-gnu-string-literal-operator-template to compiler option."
-#    define COLOR_LITERALS_CONSTEXPR constexpr
-#  else
-#    define COLOR_LITERALS_CONSTEXPR constexpr
 #  endif // !_MSC_VER
-#else
-#  define COLOR_LITERALS_CONSTEXPR const
 #endif // !TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
 
 #endif	// !TERMCOLOR2_CONFIG_HPP
