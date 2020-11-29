fae8b8a46c0298a04ad84dc1ec921d63b2e2bb96
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-11-29 20:38:12 +0900

    Remove include/poac/opts/help.hpp

--- include/poac/opts.hpp
+++ include/poac/opts.hpp
@@ -5,7 +5,6 @@
 #include <poac/opts/cache.hpp>
 #include <poac/opts/clean.hpp>
 #include <poac/opts/graph.hpp>
-#include <poac/opts/help.hpp>
 #include <poac/opts/install.hpp>
 #include <poac/opts/poac_new.hpp>
 #include <poac/opts/run.hpp>
