76522d7e1861ffc83c0163e8294048f2dfca4e5d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-01 18:45:58 +0900

    Remove clean command temporarily

--- include/poac/opts.hpp
+++ include/poac/opts.hpp
@@ -3,7 +3,6 @@
 
 #include <poac/opts/build.hpp>
 #include <poac/opts/cache.hpp>
-#include <poac/opts/clean.hpp>
 #include <poac/opts/graph.hpp>
 #include <poac/opts/install.hpp>
 #include <poac/opts/run.hpp>
