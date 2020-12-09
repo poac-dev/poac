35c33e779aaea0122d04ee4f7c5ca885d774d57c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-09 11:13:10 +0900

    Eliminate dependence on io::term from include/poac/core/resolver/resolve.hpp

--- include/poac/core/resolver/resolve.hpp
+++ include/poac/core/resolver/resolve.hpp
@@ -29,7 +29,6 @@
 #include <poac/core/name.hpp>
 #include <poac/io/config.hpp>
 #include <poac/io/lockfile.hpp>
-#include <poac/io/term.hpp>
 #include <poac/io/net.hpp>
 #include <poac/io/path.hpp>
 #include <poac/util/semver/semver.hpp>
