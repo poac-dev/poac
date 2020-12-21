db786bcd457cb874f919aead7d4dbf05cbd85033
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-21 19:22:12 +0900

    Add test for util::misc::replace

--- tests/misc.cpp
+++ tests/misc.cpp
@@ -1,11 +1,12 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/included/unit_test.hpp>
 
+// std
 #include <string>
 #include <vector>
 
+// internal
 #include <poac/util/misc.hpp>
-
 
 // std::vector<std::string> split(const std::string& raw, const std::string& delim)
 BOOST_AUTO_TEST_CASE( poac_util_misc_split_test )
@@ -19,4 +20,14 @@ BOOST_AUTO_TEST_CASE( poac_util_misc_split_test )
 
     excepted = { "Lorem ", "psum dolor s", "t amet, consectetur ad", "p", "sc", "ng el", "t, sed do e", "usmod ..." };
     BOOST_CHECK( split(test_case, "i") == excepted );
+}
+
+BOOST_AUTO_TEST_CASE( poac_util_misc_replace_test )
+{
+    using poac::util::misc::replace;
+
+    std::string test_case = "Hello world!";
+    const std::string excepted("Hello Poac!");
+    replace(test_case, "world", "Poac");
+    BOOST_CHECK( test_case == excepted );
 }
