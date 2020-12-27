d35cde3798e51546d8efb3197041fea53d6016d4
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-27 21:29:48 +0900

    Move io::path::time_to_string to util::meta

--- include/poac/io/path.hpp
+++ include/poac/io/path.hpp
@@ -2,10 +2,7 @@
 #define POAC_IO_PATH_HPP
 
 // std
-#include <ctime>
-#include <chrono>
 #include <filesystem>
-#include <string>
 
 namespace std::filesystem {
     inline namespace path_literals {
@@ -17,14 +14,5 @@ namespace std::filesystem {
 }
 
 namespace poac::io::path {
-    template <class Clock, class Duration>
-    inline std::string
-    time_to_string(const std::chrono::time_point<Clock, Duration>& time) {
-        return std::to_string(
-            std::chrono::duration_cast<std::chrono::seconds>(
-                time.time_since_epoch()
-            ).count()
-        );
-    }
 } // end namespace
 #endif // !POAC_IO_PATH_HPP
