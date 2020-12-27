e6645b7f6dc4e8c7d79d5cfe0e1dcab1b2dcac93
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-27 21:24:20 +0900

    Remove unused include statements from io::path

--- include/poac/io/path.hpp
+++ include/poac/io/path.hpp
@@ -2,19 +2,10 @@
 #define POAC_IO_PATH_HPP
 
 // std
-#include <cstdlib>
 #include <ctime>
 #include <chrono>
 #include <filesystem>
 #include <string>
-
-// external
-#include <fmt/core.h>
-#include <boost/predef.h>
-#include <mitama/result/result.hpp>
-
-// internal
-#include <poac/core/except.hpp>
 
 namespace std::filesystem {
     inline namespace path_literals {
