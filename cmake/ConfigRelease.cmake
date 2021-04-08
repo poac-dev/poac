a28cebd8342b1104e22921deb39b146c8c816edb
I <wx257osn2@yahoo.co.jp>
2021-04-09 00:22:52 +0900

    enable LTO by CMake manner

--- cmake/ConfigRelease.cmake
+++ cmake/ConfigRelease.cmake
@@ -4,7 +4,8 @@ if (APPLE)
     set(CMAKE_CXX_FLAGS_RELEASE "-O3 -flto -mtune=native -march=native")
 else ()
     set(STATIC_LINK_FLAG "-static") # ref: https://stackoverflow.com/a/3801032
-    set(CMAKE_CXX_FLAGS_RELEASE "-O3 -flto") # -mtune=powerpc64le -mcpu=powerpc64le
+    set(CMAKE_CXX_FLAGS_RELEASE "-O3") # -mtune=powerpc64le -mcpu=powerpc64le
+    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
 endif ()
 if (LINUX)
     set(Boost_USE_STATIC_LIBS ON)
