192edd3ed17070ee558c8fe270f5db7f808a5e3a
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-07-21 04:32:16 +0900

    bump toml11 version from 3.6.0 to 3.7.0

--- cmake/AddToml11.cmake
+++ cmake/AddToml11.cmake
@@ -6,7 +6,7 @@ list(APPEND CMAKE_MESSAGE_INDENT "  ")
 FetchContent_Declare(
         toml11
         GIT_REPOSITORY https://github.com/ToruNiina/toml11.git
-        GIT_TAG        v3.6.0
+        GIT_TAG        v3.7.0
 )
 
 set(toml11_BUILD_TEST OFF)
