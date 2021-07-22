7f04d3f784c59cf2afb9b1d7cf3a80595b4e8b56
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-07-23 04:27:23 +0900

    remove ssh, https, & ntlm_client from libgit2

--- cmake/AddLibGit2.cmake
+++ cmake/AddLibGit2.cmake
@@ -11,11 +11,17 @@ FetchContent_Declare(
 
 set(CMAKE_PROJECT_libgit2_INCLUDE_BEFORE "${CMAKE_SOURCE_DIR}/cmake/CMP0077PolicyFix.cmake")
 set(BUILD_CLAR OFF) # Disable testing
+set(USE_SSH OFF)
+set(USE_HTTPS OFF)
+set(USE_NTLMCLIENT OFF)
 if (CMAKE_BUILD_TYPE STREQUAL Release) # -DCMAKE_BUILD_TYPE=Release
     set(BUILD_SHARED_LIBS OFF)
 endif ()
 FetchContent_MakeAvailable(libgit2)
 unset(BUILD_CLAR)
+unset(USE_SSH)
+unset(USE_HTTPS)
+unset(USE_NTLMCLIENT)
 unset(BUILD_SHARED_LIBS)
 
 target_include_directories(${PROJECT_NAME} PRIVATE ${libgit2_SOURCE_DIR}/include)
