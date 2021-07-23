b4c4eb7f59d3c2c70f5be477e6799dd0d44fdf74
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-07-24 04:02:40 +0900

    fix a build failure issue on coverage on libarchive

--- cmake/AddLibArchive.cmake
+++ cmake/AddLibArchive.cmake
@@ -10,14 +10,30 @@ FetchContent_Declare(
 )
 
 set(CMAKE_PROJECT_libarchive_INCLUDE_BEFORE "${CMAKE_SOURCE_DIR}/cmake/LibArchivePoliciesFix.cmake")
+
 set(ENABLE_OPENSSL OFF)
+set(ENABLE_LZMA OFF)
+
+set(ENABLE_LIBXML2 OFF)
+set(ENABLE_LZMA OFF)
+
 set(ENABLE_ACL OFF)
 set(ENABLE_ICONV OFF)
 set(ENABLE_TEST OFF)
 set(ENABLE_INSTALL OFF)
 
+if (CMAKE_BUILD_TYPE STREQUAL Coverage)
+    set(CMAKE_BUILD_TYPE "Debug")
+    set(CMAKE_BUILD_TYPE_CACHE "Coverage")
+endif ()
+
 FetchContent_MakeAvailable(libarchive)
 list(APPEND POAC_DEPENDENCIES archive)
 message(CHECK_PASS "added")
+
+if (CMAKE_BUILD_TYPE_CACHE STREQUAL Coverage)
+    set(CMAKE_BUILD_TYPE "Coverage")
+    unset(CMAKE_BUILD_TYPE_CACHE)
+endif ()
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
