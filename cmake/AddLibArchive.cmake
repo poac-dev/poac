a7b5e7b50277514f84017d51541738d09b6b9b24
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2021-07-24 05:24:01 +0900

    use ENABLE_COVERAGE for enabling coverage when testing instead of CMAKE_BUILD_TYPE

--- cmake/AddLibArchive.cmake
+++ cmake/AddLibArchive.cmake
@@ -9,8 +9,6 @@ FetchContent_Declare(
         GIT_TAG        3.5.1
 )
 
-set(CMAKE_PROJECT_libarchive_INCLUDE_BEFORE "${CMAKE_SOURCE_DIR}/cmake/LibArchivePoliciesFix.cmake")
-
 set(ENABLE_OPENSSL OFF)
 set(ENABLE_LZMA OFF)
 
@@ -22,18 +20,9 @@ set(ENABLE_ICONV OFF)
 set(ENABLE_TEST OFF)
 set(ENABLE_INSTALL OFF)
 
-if (CMAKE_BUILD_TYPE STREQUAL Coverage)
-    set(CMAKE_BUILD_TYPE "Debug")
-    set(CMAKE_BUILD_TYPE_CACHE "Coverage")
-endif ()
-
+set(CMAKE_PROJECT_libarchive_INCLUDE_BEFORE "${CMAKE_SOURCE_DIR}/cmake/LibArchivePoliciesFix.cmake")
 FetchContent_MakeAvailable(libarchive)
 list(APPEND POAC_DEPENDENCIES archive)
 message(CHECK_PASS "added")
-
-if (CMAKE_BUILD_TYPE_CACHE STREQUAL Coverage)
-    set(CMAKE_BUILD_TYPE "Coverage")
-    unset(CMAKE_BUILD_TYPE_CACHE)
-endif ()
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
