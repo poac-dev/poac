7b34ea44ac23559cc8d83cccb83e25ff125265d2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-03-05 19:46:22 +0900

    Replace `clipp` with `structopt` (#467)

---
+++ cmake/AddStructopt.cmake
@@ -0,0 +1,15 @@
+include_guard(GLOBAL)
+
+message(CHECK_START "Adding structopt")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+FetchContent_Declare(
+        structopt
+        GIT_REPOSITORY https://github.com/p-ranav/structopt.git
+        GIT_TAG        v0.1.2
+)
+FetchContent_MakeAvailable(structopt)
+list(APPEND POAC_DEPENDENCIES structopt::structopt)
+message(CHECK_PASS "added")
+
+list(POP_BACK CMAKE_MESSAGE_INDENT)
