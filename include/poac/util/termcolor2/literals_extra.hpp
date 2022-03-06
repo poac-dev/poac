1048254e668ffa01f4f5f2ac6842450f28e86c7b
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-03-06 21:20:55 +0900

    Implement `termcolor2::color_literals::operator""_bold_red` (#479)

--- include/poac/util/termcolor2/literals_extra.hpp
+++ include/poac/util/termcolor2/literals_extra.hpp
@@ -7,6 +7,47 @@
 
 namespace termcolor2::inline color_literals::inline foreground_literals {
     inline std::basic_string<char>
+    operator "" _bold_red(const char* str, std::size_t len)
+    {
+        return bold_v<char>()
+             + red_v<char>()
+             + std::basic_string<char>(str, len)
+             + reset_v<char>();
+    }
+    inline std::basic_string<wchar_t>
+    operator "" _bold_red(const wchar_t* str, std::size_t len)
+    {
+        return bold_v<wchar_t>()
+             + red_v<wchar_t>()
+             + std::basic_string<wchar_t>(str, len)
+             + reset_v<wchar_t>();
+    }
+    inline std::basic_string<char8_t>
+    operator "" _bold_red(const char8_t* str, std::size_t len)
+    {
+        return bold_v<char8_t>()
+             + red_v<char8_t>()
+             + std::basic_string<char8_t>(str, len)
+             + reset_v<char8_t>();
+    }
+    inline std::basic_string<char16_t>
+    operator "" _bold_red(const char16_t* str, std::size_t len)
+    {
+        return bold_v<char16_t>()
+             + red_v<char16_t>()
+             + std::basic_string<char16_t>(str, len)
+             + reset_v<char16_t>();
+    }
+    inline std::basic_string<char32_t>
+    operator "" _bold_red(const char32_t* str, std::size_t len)
+    {
+        return bold_v<char32_t>()
+             + red_v<char32_t>()
+             + std::basic_string<char32_t>(str, len)
+             + reset_v<char32_t>();
+    }
+
+    inline std::basic_string<char>
     operator "" _bold_green(const char* str, std::size_t len)
     {
         return bold_v<char>()
