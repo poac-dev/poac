799c55ad2476ab24941a79407931769dd6b71ebd
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-03-07 01:10:21 +0900

    Implement `termcolor2::color_literals::operator""_bold_yellow` (#482)

--- include/poac/util/termcolor2/literals_extra.hpp
+++ include/poac/util/termcolor2/literals_extra.hpp
@@ -87,6 +87,47 @@ namespace termcolor2::inline color_literals::inline foreground_literals {
              + std::basic_string<char32_t>(str, len)
              + reset_v<char32_t>();
     }
+
+    inline std::basic_string<char>
+    operator "" _bold_yellow(const char* str, std::size_t len)
+    {
+        return bold_v<char>()
+             + yellow_v<char>()
+             + std::basic_string<char>(str, len)
+             + reset_v<char>();
+    }
+    inline std::basic_string<wchar_t>
+    operator "" _bold_yellow(const wchar_t* str, std::size_t len)
+    {
+        return bold_v<wchar_t>()
+             + yellow_v<wchar_t>()
+             + std::basic_string<wchar_t>(str, len)
+             + reset_v<wchar_t>();
+    }
+    inline std::basic_string<char8_t>
+    operator "" _bold_yellow(const char8_t* str, std::size_t len)
+    {
+        return bold_v<char8_t>()
+             + yellow_v<char8_t>()
+             + std::basic_string<char8_t>(str, len)
+             + reset_v<char8_t>();
+    }
+    inline std::basic_string<char16_t>
+    operator "" _bold_yellow(const char16_t* str, std::size_t len)
+    {
+        return bold_v<char16_t>()
+             + yellow_v<char16_t>()
+             + std::basic_string<char16_t>(str, len)
+             + reset_v<char16_t>();
+    }
+    inline std::basic_string<char32_t>
+    operator "" _bold_yellow(const char32_t* str, std::size_t len)
+    {
+        return bold_v<char32_t>()
+             + yellow_v<char32_t>()
+             + std::basic_string<char32_t>(str, len)
+             + reset_v<char32_t>();
+    }
 } // end namespace termcolor2::color_literals::foreground_literals
 
 #endif	// !TERMCOLOR2_LITERALS_EXTRA_HPP
