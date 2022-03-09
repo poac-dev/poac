6f5dec3fa4aa4ec9861b17257ebcdadb721471bc
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-03-10 00:06:50 +0900

    Replace `Boost.Test with `μt` in `tests/semver/token.cpp`

--- tests/semver/token.cpp
+++ tests/semver/token.cpp
@@ -1,112 +1,96 @@
-#define BOOST_TEST_MAIN
-#include <boost/predef.h>
-#include <boost/test/included/unit_test.hpp>
+#include <boost/ut.hpp>
+
 #include <poac/util/semver/parser/token.hpp>
 
-#if BOOST_OS_LINUX && BOOST_COMP_CLANG
-#  define STATIC_ASSERT BOOST_CHECK
-#else
-#  define STATIC_ASSERT static_assert
-#endif
+int main() {
+    using namespace std::literals::string_literals;
+    using namespace boost::ut;
 
-BOOST_AUTO_TEST_CASE( semver_parser_token_token_test )
-{
     using semver::parser::Token;
-    {
-        constexpr Token token{ Token::Eq };
-        STATIC_ASSERT( token == Token::Eq );
-        STATIC_ASSERT( std::holds_alternative<std::monostate>(token.component) );
-    }
-    {
-        constexpr Token token{ Token::Whitespace, 0, 2 };
-        STATIC_ASSERT( token.kind == Token::Whitespace );
-        STATIC_ASSERT( token.component == Token::variant_type{std::make_pair(0, 2)} );
-    }
-    {
-        constexpr Token token{ Token::Numeric, 0 };
-        STATIC_ASSERT( token.kind == Token::Numeric );
-        STATIC_ASSERT( token.component == Token::variant_type{std::uint_fast64_t{0}} );
-    }
-    {
-        constexpr Token token{ Token::AlphaNumeric, "beta.2" };
-        STATIC_ASSERT( token.kind == Token::AlphaNumeric );
-        STATIC_ASSERT( token.component == Token::variant_type{"beta.2"} );
-    }
-    {
-        BOOST_CHECK_THROW(
-                Token( Token::AlphaNumeric, 0, 2 ),
-                std::invalid_argument
-        );
-        BOOST_CHECK_THROW(
-                Token( Token::Whitespace, 0 ),
-                std::invalid_argument
-        );
-        BOOST_CHECK_THROW(
-                Token( Token::Numeric, "beta.2" ),
-                std::invalid_argument
-        );
-    }
-}
 
-BOOST_AUTO_TEST_CASE( semver_parser_token_is_whitespace_test )
-{
-    using semver::parser::Token;
-    STATIC_ASSERT( Token( Token::Whitespace, 0, 2 ).is_whitespace() );
-    STATIC_ASSERT( !Token( Token::Gt ).is_whitespace() );
-}
+    "test parser::Token"_test = [] {
+        {
+            constexpr Token token{ Token::Eq };
+            expect(constant<token == Token::Eq>);
+            expect(constant<std::holds_alternative<std::monostate>(token.component)>);
+        }
+        {
+            constexpr Token token{ Token::Whitespace, 0, 2 };
+            expect(constant<token.kind == Token::Whitespace>);
+            expect(constant<token.component == Token::variant_type{std::make_pair(0, 2)}>);
+        }
+        {
+            constexpr Token token{ Token::Numeric, 0 };
+            expect(constant<token.kind == Token::Numeric>);
+            expect(constant<token.component == Token::variant_type{std::uint_fast64_t{0}}>);
+        }
+        {
+            constexpr Token token{ Token::AlphaNumeric, "beta.2" };
+            expect(constant<token.kind == Token::AlphaNumeric>);
+            expect(constant<token.component == Token::variant_type{"beta.2"}>);
+        }
 
-BOOST_AUTO_TEST_CASE( semver_parser_token_is_simple_token_test )
-{
-    using semver::parser::Token;
-    STATIC_ASSERT( Token( Token::Eq ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Gt ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Lt ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::LtEq ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::GtEq ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Caret ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Tilde ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Star ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Dot ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Comma ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Hyphen ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Plus ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Or ).is_simple_token() );
-    STATIC_ASSERT( Token( Token::Unexpected ).is_simple_token() );
-    STATIC_ASSERT( !Token( Token::Numeric, 3 ).is_simple_token() );
-}
+        expect(throws<std::invalid_argument>([] {
+            Token( Token::AlphaNumeric, 0, 2 );
+        }));
+        expect(throws<std::invalid_argument>([] {
+            Token( Token::Whitespace, 0 );
+        }));
+        expect(throws<std::invalid_argument>([] {
+            Token( Token::Numeric, "beta.2" );
+        }));
+    };
 
-BOOST_AUTO_TEST_CASE( semver_parser_token_is_wildcard_test )
-{
-    using semver::parser::Token;
-    STATIC_ASSERT( Token( Token::Star ).is_wildcard() );
-    STATIC_ASSERT( Token( Token::AlphaNumeric, "x").is_wildcard() );
-    STATIC_ASSERT( Token( Token::AlphaNumeric, "X").is_wildcard() );
-    STATIC_ASSERT( !Token( Token::AlphaNumeric, "other").is_wildcard() );
-}
+    "test token is whitespace"_test = [] {
+        expect(constant<Token( Token::Whitespace, 0, 2 ).is_whitespace()>);
+        expect(constant<!Token( Token::Gt ).is_whitespace()>);
+    };
 
-BOOST_AUTO_TEST_CASE( semver_parser_token_eq_test )
-{
-    using semver::parser::Token;
-    STATIC_ASSERT( Token( Token::Star ) == Token::Star );
-    STATIC_ASSERT( Token::Star == Token( Token::Star ) );
-    STATIC_ASSERT( Token( Token::Star ) == Token( Token::Star ) );
-    STATIC_ASSERT( Token( Token::Whitespace, 0, 2 ) == Token( Token::Whitespace, 0, 2 ) );
-    STATIC_ASSERT( Token( Token::Numeric, 0 ) == Token( Token::Numeric, 0 ) );
-    STATIC_ASSERT( Token( Token::AlphaNumeric, "x") == Token( Token::AlphaNumeric, "x") );
-}
+    "test token is simple token"_test = [] {
+        expect(constant<Token( Token::Eq ).is_simple_token()>);
+        expect(constant<Token( Token::Gt ).is_simple_token()>);
+        expect(constant<Token( Token::Lt ).is_simple_token()>);
+        expect(constant<Token( Token::LtEq ).is_simple_token()>);
+        expect(constant<Token( Token::GtEq ).is_simple_token()>);
+        expect(constant<Token( Token::Caret ).is_simple_token()>);
+        expect(constant<Token( Token::Tilde ).is_simple_token()>);
+        expect(constant<Token( Token::Star ).is_simple_token()>);
+        expect(constant<Token( Token::Dot ).is_simple_token()>);
+        expect(constant<Token( Token::Comma ).is_simple_token()>);
+        expect(constant<Token( Token::Hyphen ).is_simple_token()>);
+        expect(constant<Token( Token::Plus ).is_simple_token()>);
+        expect(constant<Token( Token::Or ).is_simple_token()>);
+        expect(constant<Token( Token::Unexpected ).is_simple_token()>);
+        expect(constant<!Token( Token::Numeric, 3 ).is_simple_token()>);
+    };
 
-BOOST_AUTO_TEST_CASE( semver_parser_token_neq_test )
-{
-    using semver::parser::Token;
-    STATIC_ASSERT( Token( Token::Star ) != Token::Eq );
-    STATIC_ASSERT( Token::Star != Token( Token::Eq ) );
-    STATIC_ASSERT( Token( Token::Eq ) != Token( Token::Star ) );
-    STATIC_ASSERT( Token( Token::Whitespace, 0, 2 ) != Token::Whitespace );
-    STATIC_ASSERT( Token::Whitespace != Token( Token::Whitespace, 0, 7 ) );
-    STATIC_ASSERT( Token::Numeric != Token( Token::Numeric, 0 ) );
-    STATIC_ASSERT( Token( Token::Numeric, 8 ) != Token( Token::Numeric, 0 ) );
-    STATIC_ASSERT( Token( Token::AlphaNumeric, "x") != Token( Token::AlphaNumeric, "X") );
-    STATIC_ASSERT( Token( Token::AlphaNumeric, "x") != Token::AlphaNumeric );
-    STATIC_ASSERT( Token::Numeric != Token( Token::AlphaNumeric, "0")  );
-    STATIC_ASSERT( Token( Token::Numeric, 0 ) != Token( Token::AlphaNumeric, "0")  );
+    "test token is wildcard"_test = [] {
+        expect(constant<Token( Token::Star ).is_wildcard()>);
+        expect(constant<Token( Token::AlphaNumeric, "x").is_wildcard()>);
+        expect(constant<Token( Token::AlphaNumeric, "X").is_wildcard()>);
+        expect(constant<!Token( Token::AlphaNumeric, "other").is_wildcard()>);
+    };
+
+    "test token_eq"_test = [] {
+        expect(constant<Token( Token::Star ) == Token::Star>);
+        expect(constant<Token::Star == Token( Token::Star )>);
+        expect(constant<Token( Token::Star ) == Token( Token::Star )>);
+        expect(constant<Token( Token::Whitespace, 0, 2 ) == Token( Token::Whitespace, 0, 2 )>);
+        expect(constant<Token( Token::Numeric, 0 ) == Token( Token::Numeric, 0 )>);
+        expect(constant<Token( Token::AlphaNumeric, "x") == Token( Token::AlphaNumeric, "x")>);
+    };
+
+    "test token_neq"_test = [] {
+        expect(constant<Token( Token::Star ) != Token::Eq>);
+        expect(constant<Token::Star != Token( Token::Eq )>);
+        expect(constant<Token( Token::Eq ) != Token( Token::Star )>);
+        expect(constant<Token( Token::Whitespace, 0, 2 ) != Token::Whitespace>);
+        expect(constant<Token::Whitespace != Token( Token::Whitespace, 0, 7 )>);
+        expect(constant<Token::Numeric != Token( Token::Numeric, 0 )>);
+        expect(constant<Token( Token::Numeric, 8 ) != Token( Token::Numeric, 0 )>);
+        expect(constant<Token( Token::AlphaNumeric, "x") != Token( Token::AlphaNumeric, "X")>);
+        expect(constant<Token( Token::AlphaNumeric, "x") != Token::AlphaNumeric>);
+        expect(constant<Token::Numeric != Token( Token::AlphaNumeric, "0")>);
+        expect(constant<Token( Token::Numeric, 0 ) != Token( Token::AlphaNumeric, "0")>);
+    };
 }
