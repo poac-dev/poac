a48e53f67d0a465423e83e5b1b4df120b773b5a0
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-03-09 23:57:09 +0900

    Replace `Boost.Test with `μt` in `tests/semver/version.cpp`

--- tests/semver/version.cpp
+++ tests/semver/version.cpp
@@ -1,117 +1,109 @@
-#define BOOST_TEST_MAIN
-#include <boost/test/included/unit_test.hpp>
+#include <boost/ut.hpp>
+
 #include <poac/util/semver/version.hpp>
 #include <poac/util/semver/comparison.hpp>
 
-BOOST_AUTO_TEST_CASE( semver_eq_test )
-{
+int main() {
+    using namespace std::literals::string_literals;
+    using namespace boost::ut;
+
     using semver::Version;
 
-    BOOST_CHECK( Version("1.2.3") == "1.2.3" );
-    BOOST_CHECK( Version("1.2.3-alpha") == "1.2.3-alpha" );
-    BOOST_CHECK( Version("1.2.3+2013") == "1.2.3+2014" );
-    BOOST_CHECK( Version("1.2.3-alpha+2013") == "1.2.3-alpha+2014" );
-    BOOST_CHECK( Version("1.0.0-beta+exp.sha.5114f85") == "1.0.0-beta" );
-}
+    "test semver_eq"_test = [] {
+        expect(Version("1.2.3") == "1.2.3");
+        expect(Version("1.2.3-alpha") == "1.2.3-alpha");
+        expect(Version("1.2.3+2013") == "1.2.3+2014");
+        expect(Version("1.2.3-alpha+2013") == "1.2.3-alpha+2014");
+        expect(Version("1.0.0-beta+exp.sha.5114f85") == "1.0.0-beta");
+    };
 
-BOOST_AUTO_TEST_CASE( semver_lt_test )
-{
-    using semver::Version;
+    "test semver_lt"_test = [] {
+        expect(Version("1.2.3") < "1.2.4");
+        expect(Version("1.2.3") < "1.3.3");
+        expect(Version("1.2.3") < "2.2.3");
+        expect(Version("1.2.3") < "1.3.3");
+        expect(!(Version("9.1.1") < "1.9.9"));
+        expect(Version("1.2.3-alpha") < "1.2.3");
+        expect(Version("1.2.3-alpha") < "1.2.3-beta");
+        expect(Version("1.2.3-beta") < "1.2.3-pre");
+        expect(Version("1.2.3-beta+build") < "1.2.3-pre+build");
+        expect(Version("1.0.0-alpha") < "1.0.0-alpha.1");
+        expect(Version("1.0.0-alpha.1") < "1.0.0-alpha.beta");
+        expect(Version("1.0.0-alpha.beta") < "1.0.0-beta");
+        expect(Version("1.0.0-beta") < "1.0.0-beta.2");
+        expect(Version("1.0.0-beta.2") < "1.0.0-beta.11");
+        expect(Version("1.0.0-beta.11") < "1.0.0-rc.1");
+        expect(Version("1.0.0-rc.1") < "1.0.0");
+    };
 
-    BOOST_CHECK( Version("1.2.3") < "1.2.4" );
-    BOOST_CHECK( Version("1.2.3") < "1.3.3" );
-    BOOST_CHECK( Version("1.2.3") < "2.2.3" );
-    BOOST_CHECK( Version("1.2.3") < "1.3.3" );
-    BOOST_CHECK( !(Version("9.1.1") < "1.9.9") );
-    BOOST_CHECK( Version("1.2.3-alpha") < "1.2.3" );
-    BOOST_CHECK( Version("1.2.3-alpha") < "1.2.3-beta" );
-    BOOST_CHECK( Version("1.2.3-beta") < "1.2.3-pre" );
-    BOOST_CHECK( Version("1.2.3-beta+build") < "1.2.3-pre+build" );
-    BOOST_CHECK( Version("1.0.0-alpha") < "1.0.0-alpha.1" );
-    BOOST_CHECK( Version("1.0.0-alpha.1") < "1.0.0-alpha.beta" );
-    BOOST_CHECK( Version("1.0.0-alpha.beta") < "1.0.0-beta" );
-    BOOST_CHECK( Version("1.0.0-beta") < "1.0.0-beta.2" );
-    BOOST_CHECK( Version("1.0.0-beta.2") < "1.0.0-beta.11" );
-    BOOST_CHECK( Version("1.0.0-beta.11") < "1.0.0-rc.1" );
-    BOOST_CHECK( Version("1.0.0-rc.1") < "1.0.0" );
-}
+    "test semver_lte"_test = [] {
+        expect(Version("1.2.3") <= "1.2.3");
+        expect(Version("1.2.3-alpha") <= "1.2.3-alpha");
+        expect(Version("1.2.3+2013") <= "1.2.3+2014");
+        expect(Version("1.2.3-alpha+2013") <= "1.2.3-alpha+2014");
+        expect(Version("1.0.0-beta+exp.sha.5114f85") <= "1.0.0-beta");
 
-BOOST_AUTO_TEST_CASE( semver_lte_test )
-{
-    using semver::Version;
+        expect(Version("1.2.3") <= "1.2.4");
+        expect(Version("1.2.3") <= "1.3.3");
+        expect(Version("1.2.3") <= "2.2.3");
+        expect(Version("1.2.3") <= "1.3.3");
+        expect(!(Version("9.1.1") <= "1.9.9"));
+        expect(Version("1.2.3-alpha") <= "1.2.3");
+        expect(Version("1.2.3-alpha") <= "1.2.3-beta");
+        expect(Version("1.2.3-beta") <= "1.2.3-pre");
+        expect(Version("1.2.3-beta+build") <= "1.2.3-pre+build");
+        expect(Version("1.0.0-alpha") <= "1.0.0-alpha.1");
+        expect(Version("1.0.0-alpha.1") <= "1.0.0-alpha.beta");
+        expect(Version("1.0.0-alpha.beta") <= "1.0.0-beta");
+        expect(Version("1.0.0-beta") <= "1.0.0-beta.2");
+        expect(Version("1.0.0-beta.2") <= "1.0.0-beta.11");
+        expect(Version("1.0.0-beta.11") <= "1.0.0-rc.1");
+        expect(Version("1.0.0-rc.1") <= "1.0.0");
+    };
 
-    BOOST_CHECK( Version("1.2.3") <= "1.2.3" );
-    BOOST_CHECK( Version("1.2.3-alpha") <= "1.2.3-alpha" );
-    BOOST_CHECK( Version("1.2.3+2013") <= "1.2.3+2014" );
-    BOOST_CHECK( Version("1.2.3-alpha+2013") <= "1.2.3-alpha+2014" );
-    BOOST_CHECK( Version("1.0.0-beta+exp.sha.5114f85") <= "1.0.0-beta" );
+    "test semver_gt"_test = [] {
+        expect(Version("1.2.4") > "1.2.3");
+        expect(Version("1.3.3") > "1.2.3");
+        expect(Version("2.2.3") > "1.2.3");
+        expect(Version("1.3.3") > "1.2.3");
+        expect(!(Version("1.9.9") > "9.1.1"));
+        expect(Version("1.2.3") > "1.2.3-alpha");
+        expect(Version("1.2.3-beta") > "1.2.3-alpha");
+        expect(Version("1.2.3-pre") > "1.2.3-beta");
+        expect(Version("1.2.3-pre+build") > "1.2.3-beta+build");
+        expect(Version("1.0.0-alpha.1") > "1.0.0-alpha");
+        expect(Version("1.0.0-alpha.beta") > "1.0.0-alpha.1");
+        expect(Version("1.0.0-beta") > "1.0.0-alpha.beta");
+        expect(Version("1.0.0-beta.2") > "1.0.0-beta");
+        expect(Version("1.0.0-beta.11") > "1.0.0-beta.2");
+        expect(Version("1.0.0-rc.1") > "1.0.0-beta.11");
+        expect(Version("1.0.0") > "1.0.0-rc.1");
+    };
 
-    BOOST_CHECK( Version("1.2.3") <= "1.2.4" );
-    BOOST_CHECK( Version("1.2.3") <= "1.3.3" );
-    BOOST_CHECK( Version("1.2.3") <= "2.2.3" );
-    BOOST_CHECK( Version("1.2.3") <= "1.3.3" );
-    BOOST_CHECK( !(Version("9.1.1") <= "1.9.9") );
-    BOOST_CHECK( Version("1.2.3-alpha") <= "1.2.3" );
-    BOOST_CHECK( Version("1.2.3-alpha") <= "1.2.3-beta" );
-    BOOST_CHECK( Version("1.2.3-beta") <= "1.2.3-pre" );
-    BOOST_CHECK( Version("1.2.3-beta+build") <= "1.2.3-pre+build" );
-    BOOST_CHECK( Version("1.0.0-alpha") <= "1.0.0-alpha.1" );
-    BOOST_CHECK( Version("1.0.0-alpha.1") <= "1.0.0-alpha.beta" );
-    BOOST_CHECK( Version("1.0.0-alpha.beta") <= "1.0.0-beta" );
-    BOOST_CHECK( Version("1.0.0-beta") <= "1.0.0-beta.2" );
-    BOOST_CHECK( Version("1.0.0-beta.2") <= "1.0.0-beta.11" );
-    BOOST_CHECK( Version("1.0.0-beta.11") <= "1.0.0-rc.1" );
-    BOOST_CHECK( Version("1.0.0-rc.1") <= "1.0.0" );
-}
+    "test semver_gte"_test = [] {
+        expect(Version("1.2.3") >= "1.2.3");
+        expect(Version("1.2.3-alpha") >= "1.2.3-alpha");
+        expect(Version("1.2.3+2013") >= "1.2.3+2014");
+        expect(Version("1.2.3-alpha+2013") >= "1.2.3-alpha+2014");
+        expect(Version("1.0.0-beta+exp.sha.5114f85") >= "1.0.0-beta");
 
-BOOST_AUTO_TEST_CASE( semver_gt_test )
-{
-    using semver::Version;
-
-    BOOST_CHECK( Version("1.2.4") > "1.2.3" );
-    BOOST_CHECK( Version("1.3.3") > "1.2.3" );
-    BOOST_CHECK( Version("2.2.3") > "1.2.3" );
-    BOOST_CHECK( Version("1.3.3") > "1.2.3" );
-    BOOST_CHECK( !(Version("1.9.9") > "9.1.1") );
-    BOOST_CHECK( Version("1.2.3") > "1.2.3-alpha" );
-    BOOST_CHECK( Version("1.2.3-beta") > "1.2.3-alpha" );
-    BOOST_CHECK( Version("1.2.3-pre") > "1.2.3-beta" );
-    BOOST_CHECK( Version("1.2.3-pre+build") > "1.2.3-beta+build" );
-    BOOST_CHECK( Version("1.0.0-alpha.1") > "1.0.0-alpha" );
-    BOOST_CHECK( Version("1.0.0-alpha.beta") > "1.0.0-alpha.1" );
-    BOOST_CHECK( Version("1.0.0-beta") > "1.0.0-alpha.beta" );
-    BOOST_CHECK( Version("1.0.0-beta.2") > "1.0.0-beta" );
-    BOOST_CHECK( Version("1.0.0-beta.11") > "1.0.0-beta.2" );
-    BOOST_CHECK( Version("1.0.0-rc.1") > "1.0.0-beta.11" );
-    BOOST_CHECK( Version("1.0.0") > "1.0.0-rc.1" );
-}
-
-BOOST_AUTO_TEST_CASE( semver_gte_test )
-{
-    using semver::Version;
-
-    BOOST_CHECK( Version("1.2.3") >= "1.2.3" );
-    BOOST_CHECK( Version("1.2.3-alpha") >= "1.2.3-alpha" );
-    BOOST_CHECK( Version("1.2.3+2013") >= "1.2.3+2014" );
-    BOOST_CHECK( Version("1.2.3-alpha+2013") >= "1.2.3-alpha+2014" );
-    BOOST_CHECK( Version("1.0.0-beta+exp.sha.5114f85") >= "1.0.0-beta" );
-
-    BOOST_CHECK( Version("1.2.4") >= "1.2.3" );
-    BOOST_CHECK( Version("1.3.3") >= "1.2.3" );
-    BOOST_CHECK( Version("2.2.3") >= "1.2.3" );
-    BOOST_CHECK( Version("1.3.3") >= "1.2.3" );
-    BOOST_CHECK( !(Version("1.9.9") >= "9.1.1") );
-    BOOST_CHECK( Version("1.2.3") >= "1.2.3-alpha" );
-    BOOST_CHECK( Version("1.2.3-beta") >= "1.2.3-alpha" );
-    BOOST_CHECK( Version("1.2.3-pre") >= "1.2.3-beta" );
-    BOOST_CHECK( Version("1.2.3-pre+build") >= "1.2.3-beta+build" );
-    BOOST_CHECK( Version("1.0.0-alpha.1") >= "1.0.0-alpha" );
-    BOOST_CHECK( Version("1.0.0-alpha.beta") >= "1.0.0-alpha.1" );
-    BOOST_CHECK( Version("1.0.0-beta") >= "1.0.0-alpha.beta" );
-    BOOST_CHECK( Version("1.0.0-beta.2") >= "1.0.0-beta" );
-    BOOST_CHECK( Version("1.0.0-beta.11") >= "1.0.0-beta.2" );
-    BOOST_CHECK( Version("1.0.0-rc.1") >= "1.0.0-beta.11" );
-    BOOST_CHECK( Version("1.0.0") >= "1.0.0-rc.1" );
+        expect(Version("1.2.4") >= "1.2.3");
+        expect(Version("1.3.3") >= "1.2.3");
+        expect(Version("2.2.3") >= "1.2.3");
+        expect(Version("1.3.3") >= "1.2.3");
+        expect(!(Version("1.9.9") >= "9.1.1"));
+        expect(Version("1.2.3") >= "1.2.3-alpha");
+        expect(Version("1.2.3-beta") >= "1.2.3-alpha");
+        expect(Version("1.2.3-pre") >= "1.2.3-beta");
+        expect(Version("1.2.3-pre+build") >= "1.2.3-beta+build");
+        expect(Version("1.0.0-alpha.1") >= "1.0.0-alpha");
+        expect(Version("1.0.0-alpha.beta") >= "1.0.0-alpha.1");
+        expect(Version("1.0.0-beta") >= "1.0.0-alpha.beta");
+        expect(Version("1.0.0-beta.2") >= "1.0.0-beta");
+        expect(Version("1.0.0-beta.11") >= "1.0.0-beta.2");
+        expect(Version("1.0.0-rc.1") >= "1.0.0-beta.11");
+        expect(Version("1.0.0") >= "1.0.0-rc.1");
+    };
 }
 
 // TODO: regex, Version constructor, get_version(), get_full(), Version exceptions
