177ff7937750c8dd8bd796d0ab255b450b60e27c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-03-13 14:50:20 +0900

    Implement build system partially (#519)

--- include/poac/cmd/build.hpp
+++ include/poac/cmd/build.hpp
@@ -7,21 +7,28 @@
 #include <optional>
 
 // external
+#include <fmt/core.h>
 #include <mitama/result/result.hpp>
 #include <mitama/anyhow/anyhow.hpp>
 #include <mitama/thiserror/thiserror.hpp>
-#include <toml.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/stopwatch.h>
 #include <structopt/app.hpp>
+#include <toml.hpp>
 
 // internal
-#include <poac/core/builder.hpp>
+#include <poac/core/builder/ninja/build.hpp>
 #include <poac/core/resolver.hpp>
 #include <poac/core/validator.hpp>
 #include <poac/data/manifest.hpp>
+#include <poac/util/pretty.hpp>
 
 namespace poac::cmd::build {
     namespace anyhow = mitama::anyhow;
     namespace thiserror = mitama::thiserror;
+
+    using core::builder::ninja::build::mode_t;
+    using core::resolver::resolved_deps_t;
 
     struct Options: structopt::sub_command {
         /// Build artifacts in release mode, with optimizations
@@ -41,6 +48,31 @@ namespace poac::cmd::build {
     };
 
     [[nodiscard]] anyhow::result<std::filesystem::path>
+    build_impl(const toml::value& config, const mode_t& mode, const resolved_deps_t& resolved_deps) {
+        using termcolor2::color_literals::operator""_bold_green;
+        spdlog::info(
+            "{:>25} {} v{} ({})",
+            "Compiling"_bold_green,
+            toml::find<std::string>(config, "package", "name"),
+            toml::find<std::string>(config, "package", "version"),
+            std::filesystem::current_path().string()
+        );
+
+        spdlog::stopwatch sw;
+        const std::filesystem::path output_path = MITAMA_TRY(
+            core::builder::ninja::build::start(config, mode, resolved_deps)
+        );
+
+        spdlog::info(
+            "{:>25} {} target(s) in {}",
+            "Finished"_bold_green,
+            to_string(mode),
+            util::pretty::to_time(sw.elapsed().count())
+        );
+        return mitama::success(output_path);
+    }
+
+    [[nodiscard]] anyhow::result<std::filesystem::path>
     build(const Options& opts, const toml::value& config) {
         spdlog::trace("Resolving dependencies ...");
         const auto resolved_deps = MITAMA_TRY(
@@ -50,11 +82,9 @@ namespace poac::cmd::build {
                 })
         );
 
-        using core::builder::mode_t;
         const mode_t mode = opts.release.value() ? mode_t::release : mode_t::debug;
         const std::filesystem::path output_path = MITAMA_TRY(
-            core::builder::build(config, mode, resolved_deps)
-                .map_err([](const std::string& e){ return anyhow::anyhow(e); })
+            build_impl(config, mode, resolved_deps)
         );
         return mitama::success(output_path);
     }
--- include/poac/core/builder.hpp
+++ include/poac/core/builder.hpp
@@ -1,7 +1,6 @@
 #ifndef POAC_CORE_BUILDER_HPP
 #define POAC_CORE_BUILDER_HPP
 
-#include <poac/core/builder/builder.hpp>
 #include <poac/core/builder/ninja.hpp>
 
 #endif // POAC_CORE_BUILDER_HPP
