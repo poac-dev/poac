c51e49fb5515091e6862d30fbaf273c1a3d5a8ad
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-18 11:42:07 +0900

    Implement a login sub-command

---
+++ include/poac/cmd/login.hpp
@@ -0,0 +1,70 @@
+#ifndef POAC_CMD_LOGIN_HPP
+#define POAC_CMD_LOGIN_HPP
+
+// std
+#include <fstream>
+#include <string>
+
+// external
+#include <fmt/core.h>
+#include <mitama/result/result.hpp>
+#include <mitama/anyhow/anyhow.hpp>
+#include <mitama/thiserror/thiserror.hpp>
+#include <spdlog/spdlog.h>
+#include <structopt/app.hpp>
+
+// internal
+#include <poac/util/termcolor2/termcolor2.hpp>
+#include <poac/util/net.hpp>
+#include <poac/config.hpp>
+
+namespace poac::cmd::login {
+    namespace anyhow = mitama::anyhow;
+    namespace thiserror = mitama::thiserror;
+
+    struct Options: structopt::sub_command {
+        /// API Token obtained on poac.pm
+        std::string api_token;
+    };
+
+    class Error {
+        template <thiserror::fixed_string S, class ...T>
+        using error = thiserror::error<S, T...>;
+
+    public:
+        using InvalidAPIToken =
+            error<"invalid API token provided">;
+
+        using FailedToLogIn =
+            error<"failed to log in; API token might be incorrect">;
+    };
+
+    [[nodiscard]] anyhow::result<void>
+    exec(const Options& opts) {
+        spdlog::trace("Checking if api_token has 32 length");
+        if (opts.api_token.size() != 32) {
+            return anyhow::failure<Error::InvalidAPIToken>();
+        }
+        spdlog::trace("Checking if api_token exists");
+        if (!util::net::api::login(opts.api_token).unwrap_or(false)) {
+            return anyhow::failure<Error::FailedToLogIn>();
+        }
+
+        // Write API Token to `~/.poac/credentials` as TOML
+        spdlog::trace("Exporting the api_token to `{}`", config::path::credentials);
+        std::ofstream ofs(config::path::credentials, std::ofstream::trunc);
+        ofs << fmt::format("[registry]\ntoken = \"{}\"\n", opts.api_token);
+
+        using termcolor2::color_literals::operator""_bold_green;
+        spdlog::info(
+            "{:>25} token for `{}` saved",
+            "Login"_bold_green,
+            "poac.pm"
+        );
+        return mitama::success();
+    }
+} // end namespace
+
+STRUCTOPT(poac::cmd::login::Options, api_token);
+
+#endif // !POAC_CMD_LOGIN_HPP
