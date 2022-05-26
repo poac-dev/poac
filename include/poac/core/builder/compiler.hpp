85f4cfefe3aad514f171bf2cc2064e677844a141
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-26 15:03:16 +0900

    Retrieve implementations from cxx::cxx

--- include/poac/core/builder/compiler.hpp
+++ include/poac/core/builder/compiler.hpp
@@ -1,6 +1,6 @@
 #ifndef POAC_CORE_BUILDER_COMPILER_HPP_
 #define POAC_CORE_BUILDER_COMPILER_HPP_
 
-#include "poac/core/builder/compiler/cxx.hpp"
+#include "poac/core/builder/compiler/cxx/cxx.hpp"
 
 #endif // POAC_CORE_BUILDER_COMPILER_HPP_
