0a12ee8eb0054f383fc523f14e26413baab3f701
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-26 10:49:18 +0900

    Remove implementations for TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE

--- include/poac/util/termcolor2/literals.hpp
+++ include/poac/util/termcolor2/literals.hpp
@@ -6,18 +6,11 @@
 #include <string>  // std::basic_string
 
 // internal
+#include "poac/util/termcolor2/control.hpp"
 #include "poac/util/termcolor2/to_color.hpp"
 
 namespace termcolor2::inline color_literals::inline foreground_literals {
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, gray_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _gray() {
-  return to_gray<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _gray(const char* str, std::size_t len) {
   return to_gray(str, len);
@@ -38,16 +31,7 @@ inline std::basic_string<char32_t>
 operator"" _gray(const char32_t* str, std::size_t len) {
   return to_gray(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, red_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _red() {
-  return to_red<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _red(const char* str, std::size_t len) {
   return to_red(str, len);
@@ -68,16 +52,7 @@ inline std::basic_string<char32_t>
 operator"" _red(const char32_t* str, std::size_t len) {
   return to_red(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, green_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _green() {
-  return to_green<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _green(const char* str, std::size_t len) {
   return to_green(str, len);
@@ -98,16 +73,7 @@ inline std::basic_string<char32_t>
 operator"" _green(const char32_t* str, std::size_t len) {
   return to_green(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, yellow_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _yellow() {
-  return to_yellow<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _yellow(const char* str, std::size_t len) {
   return to_yellow(str, len);
@@ -128,16 +94,7 @@ inline std::basic_string<char32_t>
 operator"" _yellow(const char32_t* str, std::size_t len) {
   return to_yellow(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, blue_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _blue() {
-  return to_blue<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _blue(const char* str, std::size_t len) {
   return to_blue(str, len);
@@ -158,16 +115,7 @@ inline std::basic_string<char32_t>
 operator"" _blue(const char32_t* str, std::size_t len) {
   return to_blue(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, magenta_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _magenta() {
-  return to_magenta<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _magenta(const char* str, std::size_t len) {
   return to_magenta(str, len);
@@ -188,16 +136,7 @@ inline std::basic_string<char32_t>
 operator"" _magenta(const char32_t* str, std::size_t len) {
   return to_magenta(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, cyan_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _cyan() {
-  return to_cyan<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _cyan(const char* str, std::size_t len) {
   return to_cyan(str, len);
@@ -218,16 +157,7 @@ inline std::basic_string<char32_t>
 operator"" _cyan(const char32_t* str, std::size_t len) {
   return to_cyan(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, white_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _white() {
-  return to_white<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _white(const char* str, std::size_t len) {
   return to_white(str, len);
@@ -248,7 +178,6 @@ inline std::basic_string<char32_t>
 operator"" _white(const char32_t* str, std::size_t len) {
   return to_white(str, len);
 }
-#endif
 
 // clang-format off
 // to avoid reporting errors with inline namespace on only the dry-run mode. (IDK why)
@@ -256,14 +185,6 @@ operator"" _white(const char32_t* str, std::size_t len) {
 // clang-format on
 
 namespace termcolor2::inline color_literals::inline background_literals {
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, on_gray_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _on_gray() {
-  return to_on_gray<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _on_gray(const char* str, std::size_t len) {
   return to_on_gray(str, len);
@@ -284,16 +205,7 @@ inline std::basic_string<char32_t>
 operator"" _on_gray(const char32_t* str, std::size_t len) {
   return to_on_gray(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, on_red_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _on_red() {
-  return to_on_red<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _on_red(const char* str, std::size_t len) {
   return to_on_red(str, len);
@@ -314,17 +226,7 @@ inline std::basic_string<char32_t>
 operator"" _on_red(const char32_t* str, std::size_t len) {
   return to_on_red(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT,
-    on_green_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _on_green() {
-  return to_on_green<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _on_green(const char* str, std::size_t len) {
   return to_on_green(str, len);
@@ -345,17 +247,7 @@ inline std::basic_string<char32_t>
 operator"" _on_green(const char32_t* str, std::size_t len) {
   return to_on_green(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT,
-    on_yellow_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _on_yellow() {
-  return to_on_yellow<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _on_yellow(const char* str, std::size_t len) {
   return to_on_yellow(str, len);
@@ -376,16 +268,7 @@ inline std::basic_string<char32_t>
 operator"" _on_yellow(const char32_t* str, std::size_t len) {
   return to_on_yellow(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, on_blue_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _on_blue() {
-  return to_on_blue<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _on_blue(const char* str, std::size_t len) {
   return to_on_blue(str, len);
@@ -406,17 +289,7 @@ inline std::basic_string<char32_t>
 operator"" _on_blue(const char32_t* str, std::size_t len) {
   return to_on_blue(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT,
-    on_magenta_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _on_magenta() {
-  return to_on_magenta<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _on_magenta(const char* str, std::size_t len) {
   return to_on_magenta(str, len);
@@ -437,16 +310,7 @@ inline std::basic_string<char32_t>
 operator"" _on_magenta(const char32_t* str, std::size_t len) {
   return to_on_magenta(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, on_cyan_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _on_cyan() {
-  return to_on_cyan<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _on_cyan(const char* str, std::size_t len) {
   return to_on_cyan(str, len);
@@ -467,17 +331,7 @@ inline std::basic_string<char32_t>
 operator"" _on_cyan(const char32_t* str, std::size_t len) {
   return to_on_cyan(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT,
-    on_white_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _on_white() {
-  return to_on_white<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _on_white(const char* str, std::size_t len) {
   return to_on_white(str, len);
@@ -498,7 +352,6 @@ inline std::basic_string<char32_t>
 operator"" _on_white(const char32_t* str, std::size_t len) {
   return to_on_white(str, len);
 }
-#endif
 
 // clang-format off
 // to avoid reporting errors with inline namespace on only the dry-run mode. (IDK why)
@@ -506,14 +359,6 @@ operator"" _on_white(const char32_t* str, std::size_t len) {
 // clang-format on
 
 namespace termcolor2::inline color_literals::inline attribute_literals {
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, bold_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _bold() {
-  return to_bold<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _bold(const char* str, std::size_t len) {
   return to_bold(str, len);
@@ -534,16 +379,7 @@ inline std::basic_string<char32_t>
 operator"" _bold(const char32_t* str, std::size_t len) {
   return to_bold(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, dark_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _dark() {
-  return to_dark<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _dark(const char* str, std::size_t len) {
   return to_dark(str, len);
@@ -564,17 +400,7 @@ inline std::basic_string<char32_t>
 operator"" _dark(const char32_t* str, std::size_t len) {
   return to_dark(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT,
-    underline_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _underline() {
-  return to_underline<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _underline(const char* str, std::size_t len) {
   return to_underline(str, len);
@@ -595,16 +421,7 @@ inline std::basic_string<char32_t>
 operator"" _underline(const char32_t* str, std::size_t len) {
   return to_underline(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, blink_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _blink() {
-  return to_blink<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _blink(const char* str, std::size_t len) {
   return to_blink(str, len);
@@ -625,16 +442,7 @@ inline std::basic_string<char32_t>
 operator"" _blink(const char32_t* str, std::size_t len) {
   return to_blink(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT, reverse_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _reverse() {
-  return to_reverse<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _reverse(const char* str, std::size_t len) {
   return to_reverse(str, len);
@@ -655,17 +463,7 @@ inline std::basic_string<char32_t>
 operator"" _reverse(const char32_t* str, std::size_t len) {
   return to_reverse(str, len);
 }
-#endif
 
-#ifdef TERMCOLOR2_USE_GNU_STRING_LITERAL_OPERATOR_TEMPLATE
-template <typename CharT, CharT... Str>
-constexpr basic_string<
-    CharT,
-    concealed_v<CharT>().size() + sizeof...(Str) + reset_v<CharT>().size()>
-operator"" _concealed() {
-  return to_concealed<CharT, sizeof...(Str), Str...>();
-}
-#else
 inline std::basic_string<char>
 operator"" _concealed(const char* str, std::size_t len) {
   return to_concealed(str, len);
@@ -686,7 +484,6 @@ inline std::basic_string<char32_t>
 operator"" _concealed(const char32_t* str, std::size_t len) {
   return to_concealed(str, len);
 }
-#endif
 
 // clang-format off
 // to avoid reporting errors with inline namespace on only the dry-run mode. (IDK why)
