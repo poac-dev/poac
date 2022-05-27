e36d846be72c598f38cd297b8780d2ffa41d88b0
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-27 22:40:16 +0900

    Bump up structopt version

--- cmake/AddStructopt.cmake
+++ cmake/AddStructopt.cmake
@@ -6,7 +6,7 @@ list(APPEND CMAKE_MESSAGE_INDENT "  ")
 FetchContent_Declare(
         structopt
         GIT_REPOSITORY https://github.com/p-ranav/structopt.git
-        GIT_TAG        v0.1.3
+        GIT_TAG        34621d927317a7a81c46ae11fd678c0223e152a5
 )
 FetchContent_MakeAvailable(structopt)
 list(APPEND POAC_DEPENDENCIES structopt::structopt)
