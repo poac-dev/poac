21ab89ce907da31efed1864aa84f4404753779d8
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-27 19:53:02 +0900

    Fix link problem of libarchive

--- cmake/AddLibArchive.cmake
+++ cmake/AddLibArchive.cmake
@@ -4,9 +4,9 @@ message(CHECK_START "Adding LibArchive")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
 FetchContent_Declare(
-        libarchive
-        GIT_REPOSITORY https://github.com/libarchive/libarchive.git
-        GIT_TAG        v3.6.1
+    libarchive
+    GIT_REPOSITORY https://github.com/libarchive/libarchive.git
+    GIT_TAG        v3.6.1
 )
 
 set(ENABLE_OPENSSL OFF)
@@ -28,20 +28,15 @@ set(ENABLE_INSTALL OFF)
 set(CMAKE_PROJECT_libarchive_INCLUDE_BEFORE "${CMAKE_SOURCE_DIR}/cmake/LibArchivePoliciesFix.cmake")
 FetchContent_MakeAvailable(libarchive)
 
-if (APPLE)
-  set(LIBARCHIVE_LIBRARY archive)
-else ()
-  if (CMAKE_BUILD_TYPE STREQUAL Release) # -DCMAKE_BUILD_TYPE=Release
-    set(LIBARCHIVE_LIBRARY_NAME libarchive.a)
-  else ()
-    set(LIBARCHIVE_LIBRARY_NAME libarchive.so)
-  endif ()
-  set(LIBARCHIVE_LIBRARY "${libarchive_BINARY_DIR}/libarchive/${LIBARCHIVE_LIBRARY_NAME}")
-  set(LIBARCHIVE_INCLUDE_DIR ${libarchive_SOURCE_DIR}/libarchive)
-endif()
-
+set(LIBARCHIVE_INCLUDE_DIR ${libarchive_SOURCE_DIR}/libarchive)
 target_include_directories(${PROJECT_NAME} PRIVATE ${LIBARCHIVE_INCLUDE_DIR})
-list(APPEND POAC_DEPENDENCIES ${LIBARCHIVE_LIBRARY})
+if (CMAKE_BUILD_TYPE STREQUAL Debug) # -DCMAKE_BUILD_TYPE=Debug
+    set(LIBARCHIVE_LIBRARY archive)
+    list(APPEND POAC_DEPENDENCIES archive)
+else ()
+    set(LIBARCHIVE_LIBRARY archive_static)
+    list(APPEND POAC_DEPENDENCIES archive_static)
+endif ()
 
 message(CHECK_PASS "added")
 list(POP_BACK CMAKE_MESSAGE_INDENT)
