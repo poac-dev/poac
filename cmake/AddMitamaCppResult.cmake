2339ec02033665cd76ac79e3eb16ebd301ef88ab
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-27 21:57:01 +0900

    Bump up mitama-cpp-result version to v9.3.0

--- cmake/AddMitamaCppResult.cmake
+++ cmake/AddMitamaCppResult.cmake
@@ -6,7 +6,7 @@ list(APPEND CMAKE_MESSAGE_INDENT "  ")
 FetchContent_Declare(
         mitama-cpp-result
         GIT_REPOSITORY https://github.com/LoliGothick/mitama-cpp-result.git
-        GIT_TAG        ec7f22ae921f750b0115681623d0c06223737819
+        GIT_TAG        v9.3.0
 )
 FetchContent_MakeAvailable(mitama-cpp-result)
 list(APPEND POAC_DEPENDENCIES mitama-cpp-result::mitama-cpp-result)
