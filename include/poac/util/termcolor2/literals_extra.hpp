6463b12c35f33409721da342307913e24e6b18b3
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-27 17:02:15 +0900

    Add extra colors for termcolor2 (#593)

--- include/poac/util/termcolor2/literals_extra.hpp
+++ include/poac/util/termcolor2/literals_extra.hpp
@@ -6,103 +6,176 @@
 #include <string>  // std::basic_string
 
 // internal
-#include "poac/util/termcolor2/to_color.hpp"
+#include "poac/util/termcolor2/to_color_extra.hpp"
 
 namespace termcolor2::inline color_literals::inline foreground_literals {
 
 inline std::basic_string<char>
+operator"" _bold_gray(const char* str, std::size_t len) {
+  return to_bold_gray(str, len);
+}
+inline std::basic_string<wchar_t>
+operator"" _bold_gray(const wchar_t* str, std::size_t len) {
+  return to_bold_gray(str, len);
+}
+inline std::basic_string<char8_t>
+operator"" _bold_gray(const char8_t* str, std::size_t len) {
+  return to_bold_gray(str, len);
+}
+inline std::basic_string<char16_t>
+operator"" _bold_gray(const char16_t* str, std::size_t len) {
+  return to_bold_gray(str, len);
+}
+inline std::basic_string<char32_t>
+operator"" _bold_gray(const char32_t* str, std::size_t len) {
+  return to_bold_gray(str, len);
+}
+
+inline std::basic_string<char>
 operator"" _bold_red(const char* str, std::size_t len) {
-  return to_color2(
-      bold_v<char>, red_v<char>, std::basic_string<char>(str, len)
-  );
+  return to_bold_red(str, len);
 }
 inline std::basic_string<wchar_t>
 operator"" _bold_red(const wchar_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<wchar_t>, red_v<wchar_t>, std::basic_string<wchar_t>(str, len)
-  );
+  return to_bold_red(str, len);
 }
 inline std::basic_string<char8_t>
 operator"" _bold_red(const char8_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<char8_t>, red_v<char8_t>, std::basic_string<char8_t>(str, len)
-  );
+  return to_bold_red(str, len);
 }
 inline std::basic_string<char16_t>
 operator"" _bold_red(const char16_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<char16_t>, red_v<char16_t>, std::basic_string<char16_t>(str, len)
-  );
+  return to_bold_red(str, len);
 }
 inline std::basic_string<char32_t>
 operator"" _bold_red(const char32_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<char32_t>, red_v<char32_t>, std::basic_string<char32_t>(str, len)
-  );
+  return to_bold_red(str, len);
 }
 
 inline std::basic_string<char>
 operator"" _bold_green(const char* str, std::size_t len) {
-  return to_color2(
-      bold_v<char>, green_v<char>, std::basic_string<char>(str, len)
-  );
+  return to_bold_green(str, len);
 }
 inline std::basic_string<wchar_t>
 operator"" _bold_green(const wchar_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<wchar_t>, green_v<wchar_t>, std::basic_string<wchar_t>(str, len)
-  );
+  return to_bold_green(str, len);
 }
 inline std::basic_string<char8_t>
 operator"" _bold_green(const char8_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<char8_t>, green_v<char8_t>, std::basic_string<char8_t>(str, len)
-  );
+  return to_bold_green(str, len);
 }
 inline std::basic_string<char16_t>
 operator"" _bold_green(const char16_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<char16_t>, green_v<char16_t>, std::basic_string<char16_t>(str, len)
-  );
+  return to_bold_green(str, len);
 }
 inline std::basic_string<char32_t>
 operator"" _bold_green(const char32_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<char32_t>, green_v<char32_t>, std::basic_string<char32_t>(str, len)
-  );
+  return to_bold_green(str, len);
 }
 
 inline std::basic_string<char>
 operator"" _bold_yellow(const char* str, std::size_t len) {
-  return to_color2(
-      bold_v<char>, yellow_v<char>, std::basic_string<char>(str, len)
-  );
+  return to_bold_yellow(str, len);
 }
 inline std::basic_string<wchar_t>
 operator"" _bold_yellow(const wchar_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<wchar_t>, yellow_v<wchar_t>, std::basic_string<wchar_t>(str, len)
-  );
+  return to_bold_yellow(str, len);
 }
 inline std::basic_string<char8_t>
 operator"" _bold_yellow(const char8_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<char8_t>, yellow_v<char8_t>, std::basic_string<char8_t>(str, len)
-  );
+  return to_bold_yellow(str, len);
 }
 inline std::basic_string<char16_t>
 operator"" _bold_yellow(const char16_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<char16_t>, yellow_v<char16_t>,
-      std::basic_string<char16_t>(str, len)
-  );
+  return to_bold_yellow(str, len);
 }
 inline std::basic_string<char32_t>
 operator"" _bold_yellow(const char32_t* str, std::size_t len) {
-  return to_color2(
-      bold_v<char32_t>, yellow_v<char32_t>,
-      std::basic_string<char32_t>(str, len)
-  );
+  return to_bold_yellow(str, len);
+}
+
+inline std::basic_string<char>
+operator"" _bold_blue(const char* str, std::size_t len) {
+  return to_bold_blue(str, len);
+}
+inline std::basic_string<wchar_t>
+operator"" _bold_blue(const wchar_t* str, std::size_t len) {
+  return to_bold_blue(str, len);
+}
+inline std::basic_string<char8_t>
+operator"" _bold_blue(const char8_t* str, std::size_t len) {
+  return to_bold_blue(str, len);
+}
+inline std::basic_string<char16_t>
+operator"" _bold_blue(const char16_t* str, std::size_t len) {
+  return to_bold_blue(str, len);
+}
+inline std::basic_string<char32_t>
+operator"" _bold_blue(const char32_t* str, std::size_t len) {
+  return to_bold_blue(str, len);
+}
+
+inline std::basic_string<char>
+operator"" _bold_magenta(const char* str, std::size_t len) {
+  return to_bold_magenta(str, len);
+}
+inline std::basic_string<wchar_t>
+operator"" _bold_magenta(const wchar_t* str, std::size_t len) {
+  return to_bold_magenta(str, len);
+}
+inline std::basic_string<char8_t>
+operator"" _bold_magenta(const char8_t* str, std::size_t len) {
+  return to_bold_magenta(str, len);
+}
+inline std::basic_string<char16_t>
+operator"" _bold_magenta(const char16_t* str, std::size_t len) {
+  return to_bold_magenta(str, len);
+}
+inline std::basic_string<char32_t>
+operator"" _bold_magenta(const char32_t* str, std::size_t len) {
+  return to_bold_magenta(str, len);
+}
+
+inline std::basic_string<char>
+operator"" _bold_cyan(const char* str, std::size_t len) {
+  return to_bold_cyan(str, len);
+}
+inline std::basic_string<wchar_t>
+operator"" _bold_cyan(const wchar_t* str, std::size_t len) {
+  return to_bold_cyan(str, len);
+}
+inline std::basic_string<char8_t>
+operator"" _bold_cyan(const char8_t* str, std::size_t len) {
+  return to_bold_cyan(str, len);
+}
+inline std::basic_string<char16_t>
+operator"" _bold_cyan(const char16_t* str, std::size_t len) {
+  return to_bold_cyan(str, len);
+}
+inline std::basic_string<char32_t>
+operator"" _bold_cyan(const char32_t* str, std::size_t len) {
+  return to_bold_cyan(str, len);
+}
+
+inline std::basic_string<char>
+operator"" _bold_white(const char* str, std::size_t len) {
+  return to_bold_white(str, len);
+}
+inline std::basic_string<wchar_t>
+operator"" _bold_white(const wchar_t* str, std::size_t len) {
+  return to_bold_white(str, len);
+}
+inline std::basic_string<char8_t>
+operator"" _bold_white(const char8_t* str, std::size_t len) {
+  return to_bold_white(str, len);
+}
+inline std::basic_string<char16_t>
+operator"" _bold_white(const char16_t* str, std::size_t len) {
+  return to_bold_white(str, len);
+}
+inline std::basic_string<char32_t>
+operator"" _bold_white(const char32_t* str, std::size_t len) {
+  return to_bold_white(str, len);
 }
 
 // clang-format off
---
+++ include/poac/util/termcolor2/to_color_extra.hpp
@@ -0,0 +1,198 @@
+#ifndef POAC_UTIL_TERMCOLOR2_TO_COLOR_EXTRA_HPP_
+#define POAC_UTIL_TERMCOLOR2_TO_COLOR_EXTRA_HPP_
+
+// std
+#include <cstddef>     // std::size_t
+#include <string>      // std::basic_string
+#include <string_view> // std::basic_string_view
+
+// internal
+#include "poac/util/termcolor2/control.hpp"
+#include "poac/util/termcolor2/presets.hpp"
+
+namespace termcolor2 {
+
+template <typename Fn, typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_color(Fn&& fn, const std::basic_string<CharT>& str) {
+  if (should_color()) {
+    return bold_v<CharT>() + fn() + str + reset_v<CharT>();
+  } else {
+    return str;
+  }
+}
+
+//
+// Foreground manipulators
+//
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_gray(const CharT* str) {
+  return to_bold_color(gray_v<CharT>, std::basic_string<CharT>(str));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_gray(const CharT* str, std::size_t len) {
+  return to_bold_color(gray_v<CharT>, std::basic_string<CharT>(str, len));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_gray(const std::basic_string<CharT>& str) {
+  return to_bold_color(gray_v<CharT>, str);
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_gray(const std::basic_string_view<CharT> str) {
+  return to_bold_color(gray_v<CharT>, std::basic_string<CharT>(str));
+}
+
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_red(const CharT* str) {
+  return to_bold_color(red_v<CharT>, std::basic_string<CharT>(str));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_red(const CharT* str, std::size_t len) {
+  return to_bold_color(red_v<CharT>, std::basic_string<CharT>(str, len));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_red(const std::basic_string<CharT>& str) {
+  return to_bold_color(red_v<CharT>, str);
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_red(const std::basic_string_view<CharT> str) {
+  return to_bold_color(red_v<CharT>, std::basic_string<CharT>(str));
+}
+
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_green(const CharT* str) {
+  return to_bold_color(green_v<CharT>, std::basic_string<CharT>(str));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_green(const CharT* str, std::size_t len) {
+  return to_bold_color(green_v<CharT>, std::basic_string<CharT>(str, len));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_green(const std::basic_string<CharT>& str) {
+  return to_bold_color(green_v<CharT>, str);
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_green(const std::basic_string_view<CharT> str) {
+  return to_bold_color(green_v<CharT>, std::basic_string<CharT>(str));
+}
+
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_yellow(const CharT* str) {
+  return to_bold_color(yellow_v<CharT>, std::basic_string<CharT>(str));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_yellow(const CharT* str, std::size_t len) {
+  return to_bold_color(yellow_v<CharT>, std::basic_string<CharT>(str, len));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_yellow(const std::basic_string<CharT>& str) {
+  return to_bold_color(yellow_v<CharT>, str);
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_yellow(const std::basic_string_view<CharT> str) {
+  return to_bold_color(yellow_v<CharT>, std::basic_string<CharT>(str));
+}
+
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_blue(const CharT* str) {
+  return to_bold_color(blue_v<CharT>, std::basic_string<CharT>(str));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_blue(const CharT* str, std::size_t len) {
+  return to_bold_color(blue_v<CharT>, std::basic_string<CharT>(str, len));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_blue(const std::basic_string<CharT>& str) {
+  return to_bold_color(blue_v<CharT>, str);
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_blue(const std::basic_string_view<CharT> str) {
+  return to_bold_color(blue_v<CharT>, std::basic_string<CharT>(str));
+}
+
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_magenta(const CharT* str) {
+  return to_bold_color(magenta_v<CharT>, std::basic_string<CharT>(str));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_magenta(const CharT* str, std::size_t len) {
+  return to_bold_color(magenta_v<CharT>, std::basic_string<CharT>(str, len));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_magenta(const std::basic_string<CharT>& str) {
+  return to_bold_color(magenta_v<CharT>, str);
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_magenta(const std::basic_string_view<CharT> str) {
+  return to_bold_color(magenta_v<CharT>, std::basic_string<CharT>(str));
+}
+
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_cyan(const CharT* str) {
+  return to_bold_color(cyan_v<CharT>, std::basic_string<CharT>(str));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_cyan(const CharT* str, std::size_t len) {
+  return to_bold_color(cyan_v<CharT>, std::basic_string<CharT>(str, len));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_cyan(const std::basic_string<CharT>& str) {
+  return to_bold_color(cyan_v<CharT>, str);
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_cyan(const std::basic_string_view<CharT> str) {
+  return to_bold_color(cyan_v<CharT>, std::basic_string<CharT>(str));
+}
+
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_white(const CharT* str) {
+  return to_bold_color(white_v<CharT>, std::basic_string<CharT>(str));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_white(const CharT* str, std::size_t len) {
+  return to_bold_color(white_v<CharT>, std::basic_string<CharT>(str, len));
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_white(const std::basic_string<CharT>& str) {
+  return to_bold_color(white_v<CharT>, str);
+}
+template <typename CharT>
+inline TERMCOLOR2_CXX20_CONSTEVAL std::basic_string<CharT>
+to_bold_white(const std::basic_string_view<CharT> str) {
+  return to_bold_color(white_v<CharT>, std::basic_string<CharT>(str));
+}
+
+} // namespace termcolor2
+
+#endif // POAC_UTIL_TERMCOLOR2_TO_COLOR_EXTRA_HPP_
