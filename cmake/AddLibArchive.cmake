82c97656df03c89844034edfade01df624c05d76
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-29 19:32:58 +0900

    Support installing LibArchive through CPM (#606)
    * Support installing libarchive through CPM
    
    * Add LIBARCHIVE_INCLUDE_DIR

--- cmake/AddLibArchive.cmake
+++ cmake/AddLibArchive.cmake
@@ -3,43 +3,44 @@ include_guard(GLOBAL)
 message(CHECK_START "Adding LibArchive")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
-FetchContent_Declare(
-    libarchive
-    GIT_REPOSITORY https://github.com/libarchive/libarchive.git
-    GIT_TAG        v3.6.1
-)
-
-set(ENABLE_OPENSSL OFF)
-set(ENABLE_LIBB2 OFF)
-set(ENABLE_LZMA OFF)
-set(ENABLE_ZSTD OFF)
-
-set(ENABLE_ZLIB OFF)
-set(ENABLE_BZip2 OFF)
-set(ENABLE_LIBXML2 OFF)
-set(ENABLE_EXPAT OFF)
-
-# Prevent building unused executables
-set(ENABLE_CAT OFF)
-set(ENABLE_CPIO OFF)
-set(ENABLE_TAR OFF)
-
-set(ENABLE_ACL OFF)
-set(ENABLE_ICONV OFF)
-set(ENABLE_TEST OFF)
-set(ENABLE_INSTALL OFF)
+include(cmake/CPM.cmake)
 
 set(CMAKE_PROJECT_libarchive_INCLUDE_BEFORE "${CMAKE_SOURCE_DIR}/cmake/LibArchivePoliciesFix.cmake")
-FetchContent_MakeAvailable(libarchive)
+CPMAddPackage(
+    NAME LibArchive
+    GITHUB_REPOSITORY libarchive/libarchive
+    VERSION 3.6.1
+    GIT_TAG v3.6.1
+    OPTIONS
+        "ENABLE_OPENSSL OFF"
+        "ENABLE_LIBB2 OFF"
+        "ENABLE_LZMA OFF"
+        "ENABLE_ZSTD OFF"
+        "ENABLE_ZLIB OFF"
+        "ENABLE_BZip2 OFF"
+        "ENABLE_LIBXML2 OFF"
+        "ENABLE_EXPAT OFF"
+        "ENABLE_CAT OFF"
+        "ENABLE_CPIO OFF"
+        "ENABLE_TAR OFF"
+        "ENABLE_ACL OFF"
+        "ENABLE_ICONV OFF"
+        "ENABLE_TEST OFF"
+        "ENABLE_INSTALL OFF"
+)
 
-set(LIBARCHIVE_INCLUDE_DIR ${libarchive_SOURCE_DIR}/libarchive)
-target_include_directories(${PROJECT_NAME} PRIVATE ${LIBARCHIVE_INCLUDE_DIR})
-if (CMAKE_BUILD_TYPE STREQUAL Debug) # -DCMAKE_BUILD_TYPE=Debug
+if (LibArchive_ADDED) # If using downloaded package
+    set(LIBARCHIVE_INCLUDE_DIR "${LibArchive_SOURCE_DIR}/libarchive")
+    if (CMAKE_BUILD_TYPE STREQUAL Debug) # -DCMAKE_BUILD_TYPE=Debug
+        set(LIBARCHIVE_LIBRARY archive)
+        list(APPEND POAC_DEPENDENCIES archive)
+    else ()
+        set(LIBARCHIVE_LIBRARY archive_static)
+        list(APPEND POAC_DEPENDENCIES archive_static)
+    endif ()
+else () # If using local package
     set(LIBARCHIVE_LIBRARY archive)
     list(APPEND POAC_DEPENDENCIES archive)
-else ()
-    set(LIBARCHIVE_LIBRARY archive_static)
-    list(APPEND POAC_DEPENDENCIES archive_static)
 endif ()
 
 message(CHECK_PASS "added")
