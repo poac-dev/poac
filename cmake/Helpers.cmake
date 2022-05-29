e0c50b7663e2638b1b26908165f7b844f15e632f
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-05-30 02:05:49 +0900

    Remove add_poac_* macros

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -1,11 +1,3 @@
-macro (add_poac_options)
-    target_compile_options(poac PRIVATE ${ARGV})
-endmacro ()
-
-macro (add_poac_definitions)
-    target_compile_definitions(poac PRIVATE ${ARGV})
-endmacro ()
-
 # ref: https://stackoverflow.com/a/7788165
 macro (list_dir_items result curdir)
     file(GLOB items RELATIVE ${curdir} ${curdir}/*)
