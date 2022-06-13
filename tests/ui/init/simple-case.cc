c074084fb17c21728768fa02e5a7c94438f8ac1d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-13 16:41:05 +0900

    Improve UI tests

--- tests/ui/init/simple-case.cc
+++ tests/ui/init/simple-case.cc
@@ -13,6 +13,8 @@ main() {
   "simple case"_test = [] {
     const fs::path temp_dir = get_temp_dir() / "test";
     fs::create_directories(temp_dir);
-    uitest<Target::Stdout>({"init"}, temp_dir);
+    uitest<Target::Stdout>({"init"}, temp_dir, false);
+    expect(fs::exists(temp_dir / "poac.toml"));
+    remove_temp(temp_dir);
   };
 }
