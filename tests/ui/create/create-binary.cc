3dd76fed0f59fd250b4b1ef7caf13d204fe5115c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-14 18:22:16 +0900

    Add UI tests for the create command (#629)

---
+++ tests/ui/create/create-binary.cc
@@ -0,0 +1,37 @@
+// external
+#include <boost/ut.hpp>
+
+// internal
+#include "../util/ui_test_util.hpp"
+
+int
+main() {
+  using namespace boost::ut;
+  using namespace poac::util::shell;
+  using namespace poac::test;
+
+  "simple case"_test = [] {
+    const fs::path temp_dir = get_temp_dir();
+    uitest<Target::Stdout>({"create", "hello_world"}, temp_dir, false);
+
+    expect(fs::exists(temp_dir / "hello_world"));
+    expect(fs::is_directory(temp_dir / "hello_world"));
+
+    expect(fs::exists(temp_dir / "hello_world" / ".git"));
+    expect(fs::is_directory(temp_dir / "hello_world" / ".git"));
+
+    expect(fs::exists(temp_dir / "hello_world" / ".gitignore"));
+    expect(!fs::is_directory(temp_dir / "hello_world" / ".gitignore"));
+
+    expect(fs::exists(temp_dir / "hello_world" / "poac.toml"));
+    expect(!fs::is_directory(temp_dir / "hello_world" / "poac.toml"));
+
+    expect(fs::exists(temp_dir / "hello_world" / "src"));
+    expect(fs::is_directory(temp_dir / "hello_world" / "src"));
+
+    expect(fs::exists(temp_dir / "hello_world" / "src" / "main.cpp"));
+    expect(!fs::is_directory(temp_dir / "hello_world" / "src" / "main.cpp"));
+
+    remove_temp(temp_dir);
+  };
+}
