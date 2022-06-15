3ba59c1a343c86a217ab6436f743359fab0233e7
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-15 18:11:49 +0900

    Support installing `spdlog` through CPM

--- cmake/AddSpdlog.cmake
+++ cmake/AddSpdlog.cmake
@@ -3,19 +3,22 @@ include_guard(GLOBAL)
 message(CHECK_START "Adding Spdlog")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
-FetchContent_Declare(
-        spdlog
-        GIT_REPOSITORY https://github.com/gabime/spdlog.git
-        GIT_TAG        v1.10.0
-)
+include(cmake/CPM.cmake)
 
-set(SPDLOG_FMT_EXTERNAL ON)
 if (CMAKE_BUILD_TYPE STREQUAL Release) # -DCMAKE_BUILD_TYPE=Release
     set(SPDLOG_BUILD_SHARED OFF)
 else ()
     set(SPDLOG_BUILD_SHARED ON)
 endif ()
-FetchContent_MakeAvailable(spdlog)
+CPMAddPackage(
+    NAME spdlog
+    GITHUB_REPOSITORY gabime/spdlog
+    VERSION 1.10.0
+    GIT_TAG v1.10.0
+    OPTIONS
+        "SPDLOG_FMT_EXTERNAL ON"
+        "SPDLOG_BUILD_SHARED ${SPDLOG_BUILD_SHARED}"
+)
 
 list(APPEND POAC_DEPENDENCIES spdlog::spdlog)
 message(CHECK_PASS "added")
