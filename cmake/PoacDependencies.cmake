65010872ba0e61f8e2f36b5b82926be3811e48ce
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-20 14:32:57 +0900

    Remove `POAC_DEPS_DIR`

--- cmake/PoacDependencies.cmake
+++ cmake/PoacDependencies.cmake
@@ -5,15 +5,6 @@ message(CHECK_START "Adding Poac dependencies")
 list(APPEND CMAKE_MESSAGE_INDENT "  ")
 unset(missingDependencies)
 
-if (DEFINED POAC_DEPS_DIR)
-    if (NOT DEFINED BOOST_ROOT)
-        set(BOOST_ROOT "${POAC_DEPS_DIR}/boost")
-    endif ()
-    if (NOT DEFINED LIBGIT2_DIR)
-        set(LIBGIT2_DIR "${POAC_DEPS_DIR}/libgit2")
-    endif ()
-endif ()
-
 include(FetchContent)
 list_dir_items(DEPENDENCIES ${CMAKE_SOURCE_DIR}/cmake)
 list(FILTER DEPENDENCIES INCLUDE REGEX "Add.*cmake")  # Add files that match with the regex
