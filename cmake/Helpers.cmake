aa5b8341a75d31f6a429c0d3565a058ab73610c8
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-21 20:44:10 +0900

    Avoid providing poac libraries

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -16,3 +16,35 @@ if (APPLE)
     get_homebrew_prefix_path()
     set(POAC_HOMEBREW_ROOT_PATH "${POAC_HOMEBREW_PREFIX_PATH}/opt")
 endif()
+
+# If no sources are provided, then only an interface target will be added.
+function (add_poac_target target_name)
+    cmake_parse_arguments(VARS "" "" "SOURCES;INCLUDES;LIBRARIES;DEFINES" ${ARGN})
+
+    add_library(${target_name} INTERFACE) # prepare an interface target for dependents
+    target_compile_features(${target_name} INTERFACE cxx_std_20)
+    target_compile_definitions(${target_name} INTERFACE ${VARS_DEFINES})
+    target_include_directories(${target_name} INTERFACE ${VARS_INCLUDES})
+
+    list(LENGTH VARS_SOURCES SOURCE_COUNT)
+    if (${SOURCE_COUNT} GREATER 0)
+        add_library(${target_name}_obj OBJECT ${VARS_SOURCES})
+        target_compile_features(${target_name}_obj PUBLIC cxx_std_20)
+        target_compile_definitions(${target_name}_obj PUBLIC ${VARS_DEFINES})
+
+        target_include_directories(${target_name}_obj PUBLIC ${VARS_INCLUDES})
+        target_link_libraries(${target_name}_obj PUBLIC ${STATIC_LINK_FLAG} ${VARS_LIBRARIES})
+
+        target_link_libraries(${target_name} INTERFACE
+            ${STATIC_LINK_FLAG}
+            ${VARS_LIBRARIES}
+            ${target_name}_obj
+            $<TARGET_OBJECTS:${target_name}_obj>
+        )
+    else ()
+        target_link_libraries(${target_name} INTERFACE
+            ${STATIC_LINK_FLAG}
+            ${VARS_LIBRARIES}
+        )
+    endif ()
+endfunction ()
