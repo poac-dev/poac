6638876acf7424f9a53d36d9cdffb476ac5e77fe
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-07-26 21:50:43 +0900

    Support formatting drogon projects (#665)

--- include/poac/cmd/fmt.hpp
+++ include/poac/cmd/fmt.hpp
@@ -10,22 +10,12 @@
 namespace poac::cmd::fmt {
 
 struct Options : structopt::sub_command {
-  // Perform only checks
+  /// Perform only checks
   Option<bool> check = false;
+
+  /// Format a drogon project
+  Option<bool> drogon = false;
 };
-
-using ClangFormatNotFound = Error<
-    "`fmt` command requires `clang-format`; try installing it by:\n"
-    "  apt/brew install clang-format">;
-
-inline constexpr StringRef directories[] = {
-    "examples", "include", "lib", "src", "tests"};
-
-inline constexpr StringRef extensions[] = {"c",   "c++", "cc",  "cpp",
-                                           "cu",  "cuh", "cxx", "h",
-                                           "h++", "hh",  "hpp", "hxx"};
-
-inline constexpr StringRef patterns[] = {"./{}/*.{}", "./{}/**/*.{}"};
 
 [[nodiscard]] Result<void>
 fmt(StringRef args);
@@ -35,6 +25,6 @@ exec(const Options& opts);
 
 } // namespace poac::cmd::fmt
 
-STRUCTOPT(poac::cmd::fmt::Options, check);
+STRUCTOPT(poac::cmd::fmt::Options, check, drogon);
 
 #endif // POAC_CMD_FMT_HPP_
