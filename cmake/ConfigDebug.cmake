63dadd43ba47c51260c5a937c7664cc99a4d51bf
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-11-23 16:42:53 -0800

    Enable clang-tidy only when debug (#699)

--- cmake/ConfigDebug.cmake
+++ cmake/ConfigDebug.cmake
@@ -4,3 +4,8 @@ include_guard(GLOBAL)
 set(CMAKE_CXX_FLAGS_DEBUG "-g -O1 -fsanitize=address -fno-omit-frame-pointer")
 # https://stackoverflow.com/a/38297422
 set(CMAKE_POSITION_INDEPENDENT_CODE ON) # -fPIC
+
+if (POAC_CLANG_TIDY)
+    # Enable clang-tidy
+    set(CMAKE_CXX_CLANG_TIDY clang-tidy)
+endif ()
