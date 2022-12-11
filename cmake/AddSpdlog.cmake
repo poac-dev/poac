14ae95b332c24030be0ea0adc3e2d7f24500eb66
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-12-10 21:16:41 -0800

    Use proper minimum required version for spdlog

--- cmake/AddSpdlog.cmake
+++ cmake/AddSpdlog.cmake
@@ -13,7 +13,7 @@ endif ()
 CPMAddPackage(
     NAME spdlog
     GITHUB_REPOSITORY gabime/spdlog
-    VERSION 1.10.0
+    VERSION 1.9.0 # minimum required version
     GIT_TAG v1.10.0
     OPTIONS
         "SPDLOG_FMT_EXTERNAL ON"
