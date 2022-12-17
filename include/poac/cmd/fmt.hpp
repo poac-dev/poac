81b575b17c6354ebec03d2962a533969f3d52658
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-12-17 05:31:57 -0800

    Comprehensively find format target directories (#724)

--- include/poac/cmd/fmt.hpp
+++ include/poac/cmd/fmt.hpp
@@ -14,13 +14,10 @@ namespace poac::cmd::fmt {
 struct Options : structopt::sub_command {
   /// Perform only checks
   Option<bool> check = false;
-
-  /// Format a drogon project
-  Option<bool> drogon = false;
 };
 
 [[nodiscard]] Fn exec(const Options& opts)->Result<void>;
 
 } // namespace poac::cmd::fmt
 
-STRUCTOPT(poac::cmd::fmt::Options, check, drogon);
+STRUCTOPT(poac::cmd::fmt::Options, check);
