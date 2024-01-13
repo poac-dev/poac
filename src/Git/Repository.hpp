2eddbb1f4a2284856a280288834ff52508dcfa19
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-01-12 19:10:16 -0800

    repository

--- src/Git/Repository.hpp
+++ src/Git/Repository.hpp
@@ -2,12 +2,8 @@
 
 #include "../Rustify.hpp"
 #include "Config.hpp"
-#include "Exception.hpp"
-#include "Global.hpp"
 
-#include <git2/ignore.h>
 #include <git2/repository.h>
-#include <string>
 
 namespace git2 {
 
@@ -15,9 +11,7 @@ struct Repository {
   git_repository* raw = nullptr;
 
   Repository() = default;
-  ~Repository() {
-    git_repository_free(this->raw);
-  }
+  ~Repository();
 
   Repository(const Repository&) = delete;
   Repository& operator=(const Repository&) = delete;
@@ -27,58 +21,32 @@ struct Repository {
   /// Attempt to open an already-existing repository at `path`.
   ///
   /// The path can point to either a normal or bare repository.
-  Repository& open(const StringRef path) {
-    git2::init();
-    git2Throw(git_repository_open(&this->raw, path.data()));
-    return *this;
-  }
+  Repository& open(const StringRef path);
   /// Attempt to open an already-existing bare repository at `path`.
   ///
   /// The path can point to only a bare repository.
-  Repository& openBare(const StringRef path) {
-    git2::init();
-    git2Throw(git_repository_open_bare(&this->raw, path.data()));
-    return *this;
-  }
+  Repository& openBare(const StringRef path);
 
   /// Creates a new repository in the specified folder.
   ///
   /// This by default will create any necessary directories to create the
   /// repository, and it will read any user-specified templates when creating
   /// the repository. This behavior can be configured through `init_opts`.
-  Repository& init(const StringRef path) {
-    git2::init();
-    git2Throw(git_repository_init(&this->raw, path.data(), false));
-    return *this;
-  }
+  Repository& init(const StringRef path);
   /// Creates a new `--bare` repository in the specified folder.
   ///
   /// The folder must exist prior to invoking this function.
-  Repository& initBare(const StringRef path) {
-    git2::init();
-    git2Throw(git_repository_init(&this->raw, path.data(), true));
-    return *this;
-  }
+  Repository& initBare(const StringRef path);
 
   /// Check if path is ignored by the ignore rules.
-  bool isIgnored(const StringRef path) const {
-    git2::init();
-
-    int ignored = 0;
-    git2Throw(git_ignore_path_is_ignored(&ignored, this->raw, path.data()));
-    return static_cast<bool>(ignored);
-  }
+  bool isIgnored(const StringRef path) const;
 
   /// Get the configuration file for this repository.
   ///
   /// If a configuration file has not been set, the default config set for
   /// the repository will be returned, including global and system
   /// configurations (if they are available).
-  git2::Config config() const {
-    git_config* cfg = nullptr;
-    git2Throw(git_repository_config(&cfg, this->raw));
-    return git2::Config(cfg);
-  }
+  git2::Config config() const;
 };
 
 } // end namespace git2
