f3093a8a55621a8892a7d123431c8cae9a97dbc2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-01-18 21:39:31 -0800

    Move src/Git2/Git2.hpp to src

---
+++ src/Git2.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Git2/Config.hpp"
+#include "Git2/Describe.hpp"
+#include "Git2/Exception.hpp"
+#include "Git2/Global.hpp"
+#include "Git2/Oid.hpp"
+#include "Git2/Repository.hpp"
+#include "Git2/Revparse.hpp"
+#include "Git2/Revwalk.hpp"
