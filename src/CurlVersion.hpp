19c66a235522d5163d4f20e0718e066f012814cf
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-01-20 18:56:32 -0800

    Handle unavailable version info

---
+++ src/CurlVersion.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <curl/curl.h>
+#include <ostream>
+
+namespace curl {
+
+struct Version {
+  curl_version_info_data* data;
+
+  Version() : data(curl_version_info(CURLVERSION_NOW)) {}
+};
+
+std::ostream& operator<<(std::ostream& os, const Version& version);
+
+}; // namespace curl
