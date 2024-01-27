b19bfa749fb890734e51f29847e5e48602e59385
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2024-01-26 22:22:38 -0800

    constexpr levDistance

--- src/Algos.cc
+++ src/Algos.cc
@@ -77,85 +77,6 @@ commandExists(const StringRef cmd) noexcept {
   return execCmd(checkCmd) == EXIT_SUCCESS;
 }
 
-// ref: https://wandbox.org/permlink/zRjT41alOHdwcf00
-static usize
-levDistance(const StringRef lhs, const StringRef rhs) {
-  const usize lhsSize = lhs.size();
-  const usize rhsSize = rhs.size();
-
-  // for all i and j, d[i,j] will hold the Levenshtein distance between the
-  // first i characters of s and the first j characters of t
-  Vec<Vec<usize>> dist(lhsSize + 1, Vec<usize>(rhsSize + 1));
-  dist[0][0] = 0;
-
-  // source prefixes can be transformed into empty string by dropping all
-  // characters
-  for (usize i = 1; i <= lhsSize; ++i) {
-    dist[i][0] = i;
-  }
-
-  // target prefixes can be reached from empty source prefix by inserting every
-  // character
-  for (usize j = 1; j <= rhsSize; ++j) {
-    dist[0][j] = j;
-  }
-
-  for (usize i = 1; i <= lhsSize; ++i) {
-    for (usize j = 1; j <= rhsSize; ++j) {
-      const usize substCost = lhs[i - 1] == rhs[j - 1] ? 0 : 1;
-      dist[i][j] = std::min({
-          dist[i - 1][j] + 1, // deletion
-          dist[i][j - 1] + 1, // insertion
-          dist[i - 1][j - 1] + substCost // substitution
-      });
-    }
-  }
-
-  return dist[lhsSize][rhsSize];
-}
-
-static bool
-equalsInsensitive(const StringRef lhs, const StringRef rhs) {
-  return std::equal(
-      lhs.cbegin(), lhs.cend(), rhs.cbegin(), rhs.cend(),
-      [](char lhs, char rhs) { return std::tolower(lhs) == std::tolower(rhs); }
-  );
-}
-
-Option<StringRef>
-findSimilarStr(const StringRef lhs, std::span<const StringRef> candidates) {
-  // We need to check if `Candidates` has the exact case-insensitive string
-  // because the Levenshtein distance match does not care about it.
-  for (const StringRef str : candidates) {
-    if (equalsInsensitive(lhs, str)) {
-      return str;
-    }
-  }
-
-  // Keep going with the Levenshtein distance match.
-  // If the LHS size is less than 3, use the LHS size minus 1 and if not,
-  // use the LHS size divided by 3.
-  const usize length = lhs.size();
-  const usize maxDist = length < 3 ? length - 1 : length / 3;
-
-  Option<std::pair<StringRef, usize>> similarStr = None;
-  for (const StringRef str : candidates) {
-    const usize curDist = levDistance(lhs, str);
-    if (curDist <= maxDist) {
-      // The first similar string found || More similar string found
-      if (!similarStr.has_value() || curDist < similarStr->second) {
-        similarStr = { str, curDist };
-      }
-    }
-  }
-
-  if (similarStr.has_value()) {
-    return similarStr->first;
-  } else {
-    return None;
-  }
-}
-
 #ifdef POAC_TEST
 
 #  include <limits>
@@ -179,22 +100,22 @@ testLevDistance2() {
   constexpr StringRef STR2 = "\nMary häd ä little lämb\n\nLittle lämb\n";
   constexpr StringRef STR3 = "Mary häd ä little lämb\n\nLittle lämb\n";
 
-  assertEq(levDistance(STR1, STR2), 2UL);
-  assertEq(levDistance(STR2, STR1), 2UL);
-  assertEq(levDistance(STR1, STR3), 3UL);
-  assertEq(levDistance(STR3, STR1), 3UL);
-  assertEq(levDistance(STR2, STR3), 1UL);
-  assertEq(levDistance(STR3, STR2), 1UL);
+  static_assert(levDistance(STR1, STR2) == 2UL);
+  static_assert(levDistance(STR2, STR1) == 2UL);
+  static_assert(levDistance(STR1, STR3) == 3UL);
+  static_assert(levDistance(STR3, STR1) == 3UL);
+  static_assert(levDistance(STR2, STR3) == 1UL);
+  static_assert(levDistance(STR3, STR2) == 1UL);
 
-  assertEq(levDistance("b", "bc"), 1UL);
-  assertEq(levDistance("ab", "abc"), 1UL);
-  assertEq(levDistance("aab", "aabc"), 1UL);
-  assertEq(levDistance("aaab", "aaabc"), 1UL);
+  static_assert(levDistance("b", "bc") == 1UL);
+  static_assert(levDistance("ab", "abc") == 1UL);
+  static_assert(levDistance("aab", "aabc") == 1UL);
+  static_assert(levDistance("aaab", "aaabc") == 1UL);
 
-  assertEq(levDistance("a", "b"), 1UL);
-  assertEq(levDistance("ab", "ac"), 1UL);
-  assertEq(levDistance("aab", "aac"), 1UL);
-  assertEq(levDistance("aaab", "aaac"), 1UL);
+  static_assert(levDistance("a", "b") == 1UL);
+  static_assert(levDistance("ab", "ac") == 1UL);
+  static_assert(levDistance("aab", "aac") == 1UL);
+  static_assert(levDistance("aaab", "aaac") == 1UL);
 
   pass();
 }
@@ -208,20 +129,22 @@ testFindSimilarStr() {
     "if", "ifdef", "ifndef", "elif", "else", "endif", "elifdef", "elifndef"
   };
 
-  assertEq(findSimilarStr("id", CANDIDATES), "if"sv);
-  assertEq(findSimilarStr("ifd", CANDIDATES), "if"sv);
-  assertEq(findSimilarStr("ifde", CANDIDATES), "ifdef"sv);
-  assertEq(findSimilarStr("elf", CANDIDATES), "elif"sv);
-  assertEq(findSimilarStr("elsif", CANDIDATES), "elif"sv);
-  assertEq(findSimilarStr("elseif", CANDIDATES), "elif"sv);
-  assertEq(findSimilarStr("elfidef", CANDIDATES), "elifdef"sv);
-  assertEq(findSimilarStr("elfindef", CANDIDATES), "elifdef"sv);
-  assertEq(findSimilarStr("elfinndef", CANDIDATES), "elifndef"sv);
-  assertEq(findSimilarStr("els", CANDIDATES), "else"sv);
-  assertEq(findSimilarStr("endi", CANDIDATES), "endif"sv);
+  static_assert(findSimilarStr("id", CANDIDATES) == "if"sv);
+  static_assert(findSimilarStr("ifd", CANDIDATES) == "if"sv);
+  static_assert(findSimilarStr("ifde", CANDIDATES) == "ifdef"sv);
+  static_assert(findSimilarStr("elf", CANDIDATES) == "elif"sv);
+  static_assert(findSimilarStr("elsif", CANDIDATES) == "elif"sv);
+  static_assert(findSimilarStr("elseif", CANDIDATES) == "elif"sv);
+  static_assert(findSimilarStr("elfidef", CANDIDATES) == "elifdef"sv);
+  static_assert(findSimilarStr("elfindef", CANDIDATES) == "elifdef"sv);
+  static_assert(findSimilarStr("elfinndef", CANDIDATES) == "elifndef"sv);
+  static_assert(findSimilarStr("els", CANDIDATES) == "else"sv);
+  static_assert(findSimilarStr("endi", CANDIDATES) == "endif"sv);
 
-  assertEq(findSimilarStr("i", CANDIDATES), None);
-  assertEq(findSimilarStr("special_compiler_directive", CANDIDATES), None);
+  static_assert(findSimilarStr("i", CANDIDATES) == None);
+  static_assert(
+      findSimilarStr("special_compiler_directive", CANDIDATES) == None
+  );
 
   pass();
 }
@@ -229,11 +152,11 @@ testFindSimilarStr() {
 void
 testFindSimilarStr2() {
   constexpr Arr<StringRef, 2> CANDIDATES{ "aaab", "aaabc" };
-  assertEq(findSimilarStr("aaaa", CANDIDATES), "aaab"sv);
-  assertEq(findSimilarStr("1111111111", CANDIDATES), None);
+  static_assert(findSimilarStr("aaaa", CANDIDATES) == "aaab"sv);
+  static_assert(findSimilarStr("1111111111", CANDIDATES) == None);
 
   constexpr Arr<StringRef, 1> CANDIDATES2{ "AAAA" };
-  assertEq(findSimilarStr("aaaa", CANDIDATES2), "AAAA"sv);
+  static_assert(findSimilarStr("aaaa", CANDIDATES2) == "AAAA"sv);
 
   pass();
 }
