fa1ec9a98a2e6a17ba88ce4ed0e80cbaf11de203
How2PlayGithub <how2bypass@gmail.com>
2024-05-08 22:09:27 +1200

    fixed dependencies with .'s in them for versioning

--- src/Manifest.cc
+++ src/Manifest.cc
@@ -428,7 +428,7 @@ validateDepName(const StringRef name) {
             c == '.' && std::isdigit(name[&c - &name[0] - 1])
             && std::isdigit(name[&c - &name[0] + 1])
         )) {
-      throw PoacError("dependency name must be alphanumeric, `-`, `_`, or `/`");
+      throw PoacError("dependency name must be alphanumeric, `-`, `_`, `/` or `.` for versioning");
     }
   }
 
