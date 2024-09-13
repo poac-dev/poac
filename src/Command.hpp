10cdb23d1ec43baf99d3e4ddcbf315d3c9e6ec5f
Зишан Мирза <149377404+zimirza@users.noreply.github.com>
2024-09-13 20:22:32 +0200

    refactor: use `std::string`

--- src/Command.hpp
+++ src/Command.hpp
@@ -1,35 +1,27 @@
 #pragma once
 
+#include <cstdio>
 #include <iostream>
 #include <string>
-#include <cstring>
 
 class Command
 {
 private:
-    char* buf;
-    int len;
+    std::string buf;
 public:
-    Command(const char *arg) {
-        len = 0;
-        buf = (char *) malloc(sizeof(char) * strlen(arg));
-        if (buf == NULL) {
-            throw std::runtime_error("unable to allocate memory");
-        }
-        len += snprintf(buf+len, 3, "%s", arg);
+    Command(std::string arg) {
+        buf.append(arg);
     }
-    Command arg(const char *arg) {
-        buf = (char *) realloc(buf, strlen(buf)+strlen(arg));
-        if (buf == NULL) {
-            throw std::runtime_error("unable to allocate memory");
-        }
-        len += snprintf(buf+len, strlen(buf)+strlen(arg), " %s", arg);
+    ~Command() {
+    }
+    Command arg(std::string arg) {
+        buf.append(" ");
+        buf.append(arg);
         return *this;
     }
     Command output() {
-        fprintf(stdout, "%s\n", buf);
-        system(buf);
-        free(buf);
+        std::cout << buf << std::endl;
+        system(buf.data());
         return *this;
     }
 };
