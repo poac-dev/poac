04c8551a198e82e235c52ba8f42d16c1d6dae7d8
yaito3014 <ykakeyama3014@gmail.com>
2024-09-14 18:25:43 +0900

    replace system() with execvp()

--- src/Command.cc
+++ src/Command.cc
@@ -7,13 +7,48 @@
 #include <array>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
 
 int
 Command::execute() const {
   const std::string cmdline = getCmdline();
-  logger::debug("Running `", cmdline, '`');
-  const int status = system(cmdline.c_str());
-  return WEXITSTATUS(status);
+
+  int fd[2];
+
+  if (pipe(fd)) {
+    throw PoacError("pipe() failed");
+  }
+
+  pid_t pid = fork();
+
+  if (pid == -1) {
+    close(fd[0]);
+    close(fd[1]);
+    throw PoacError("fork() failed");
+  } else if (pid == 0) {
+    std::vector<char*> args;
+    args.push_back(const_cast<std::string&>(command).data());
+    for (std::string& arg : const_cast<std::vector<std::string>&>(arguments)) {
+      args.push_back(arg.data());
+    }
+    args.push_back(nullptr);
+    if (execvp(command.data(), args.data()) == -1) {
+      close(fd[1]);
+      throw PoacError("execvp() failed");
+    }
+    close(fd[1]);
+    exit(EXIT_SUCCESS);
+  } else {
+    int status;
+    if (waitpid(pid, &status, 0) == -1) {
+      close(fd[0]);
+      throw PoacError("waitpid() failed");
+    }
+    close(fd[0]);
+    return WEXITSTATUS(status);
+  }
 }
 
 CommandOutput
